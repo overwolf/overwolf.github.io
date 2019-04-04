5b6f19d21e1771856cc39b742513591db322e77a
erez.efrat <erez.efrat@overwolf.com>
2019-04-04 14:04:29 +0300

    fixed siteConfig.js (broke after merging pull request)

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,14 +23,14 @@ const users = [
 const siteConfig = {
   title: 'Overwolf Developers', // Title for your website.
   tagline: 'Easily create apps for PC gamers',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://overwolf.github.io', // Your website URL
+  baseUrl: '/developers-site/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'dev-site',
+  projectName: 'developers-site',
   organizationName: 'overwolf',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
