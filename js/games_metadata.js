1d0a9100df98ff43bd1be963df8ca6d7c5e05f30
fullmetalboy <fullmetalboy@users.noreply.github.com>
2019-06-26 15:57:23 +0000

    Deploy website
    Deploy website version based on b9abb5016e784c34aacadc70206742e411343e26

--- js/games_metadata.js
+++ js/games_metadata.js
@@ -15,91 +15,91 @@ const GamesMetadata = {
   },
   6365: {
     id: 6365,
-    path: "#",
+    path: "/developers-site/docs/status/world-of-tanks",
     iconUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/wot.png",
     iconLargeUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/large/wot.png",
     name: "World Of Tanks"
   },
   10826: {
     id: 10826,
-    path: "#",
+    path: "/developers-site/docs/status/rainbow-six-siege",
     iconUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/rainbow_six_siege.png",
     iconLargeUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/large/rainbow_six_siege.png",
     name: "Rainbow Six: Siege"
   },
   21404: {
     id: 21404,
-    path: "#",
+    path: "/developers-site/docs/status/splitgate",
     iconUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/splitgate.png",
     iconLargeUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/large/splitgate.png",
     name: "Splitgate: Arena Warfare"
   },
   10798: {
     id: 10798,
-    path: "#",
+    path: "/developers-site/docs/status/rocket-league",
     iconUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/rocket_league.png",
     iconLargeUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/large/rocket_league.png",
     name: "Rocket League"
   },
   10906: {
     id: 10906,
-    path: "#",
+    path: "/developers-site/docs/status/pubg",
     iconUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/pubg.png",
     iconLargeUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/large/pubg.png",
     name: "PUBG"
   },
   10844: {
     id: 10844,
-    path: "#",
+    path: "/developers-site/docs/status/overwatch",
     iconUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/overwatch.png",
     iconLargeUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/large/overwatch.png",
     name: "Overwatch"
   },
   5426: {
     id: 5426,
-    path: "#",
+    path: "/developers-site/docs/status/lol",
     iconUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/lol.png",
     iconLargeUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/large/lol.png",
     name: "League of Legends"
   },
   9898: {
     id: 9898,
-    path: "#",
+    path: "/developers-site/docs/status/hearthstone",
     iconUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/hearthstone.png",
     iconLargeUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/large/hearthstone.png",
     name: "Hearthstone"
   },
   21216: {
     id: 21216,
-    path: "#",
+    path: "/developers-site/docs/status/fortnite",
     iconUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/fortnite.png",
     iconLargeUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/large/fortnite.png",
     name: "Fortnite"
   },
   21568: {
     id: 21568,
-    path: "#",
+    path: "/developers-site/docs/status/auto-chess",
     iconUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/autochess.png",
     iconLargeUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/large/autochess.png",
     name: "Auto-Chess"
   },
   7314: {
     id: 7314,
-    path: "#",
+    path: "/developers-site/docs/status/dota2",
     iconUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/dota2.png",
     iconLargeUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/large/dota2.png",
     name: "Dota 2"
   },
   7764: {
     id: 7764,
-    path: "#",
+    path: "/developers-site/docs/status/csgo",
     iconUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/cs_go.png",
     iconLargeUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/large/cs_go.png",
     name: "CS: GO"
   },
   21566: {
     id: 21566,
-    path: "#",
+    path: "/developers-site/docs/status/apex",
     iconUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/apex.png",
     iconLargeUrl: "http://developers.overwolf.com/wp-content/themes/owdev/images/games/large/apex.png",
     name: "Apex Legends"
