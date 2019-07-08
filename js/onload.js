426c696c39fbcc55d7fa56bb476b93382afe2b3d
fullmetalboy <fullmetalboy@users.noreply.github.com>
2019-07-08 09:48:44 +0000

    Deploy website
    Deploy website version based on a97bfabba3e40c15262d73cdd2844463f1eee602

---
+++ js/onload.js
@@ -0,0 +1,19 @@
+function addSiderbarHighlight() {
+  const selectedListItem = $(".navListItemActive");
+  if (!selectedListItem) return;
+  const listParent = selectedListItem.parents('ul').last().parent();
+  const categoryTitle = listParent.children('.navGroupCategoryTitle').last();
+  categoryTitle.css('border-left', '4px solid #E5004C');
+}
+
+function moveEditButton() {
+  const button = document.getElementsByClassName('edit-page-link')[0]
+  if (!button) return;
+  const pageNav = document.getElementsByClassName('onPageNav')[0];
+  pageNav.appendChild(button);
+}
+
+window.onload = function () {
+  addSiderbarHighlight();
+  moveEditButton();
+};
