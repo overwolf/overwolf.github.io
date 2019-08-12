d946557568123b045af852bbd727dac88fba1deb
eransharv <eransharv@gmail.com>
2019-08-12 10:47:47 +0300

    fix the homepage links

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -8,7 +8,7 @@ function EasilySection(props) {
       <div className="container">
         <header>Easily create apps for PC gamers</header>
         <p>Overlay, real-time game events, monetization, auto patching, Anti-Cheat compatibility and all that good stuff</p>
-        <a className="btn" href={`${baseUrl}docs/start/getting-started`}>Create app</a>
+        <a className="btn" href={`${baseUrl}docs/start/submit-app-proposal`}>Submit an App Proposal</a>
       </div>
       <img className="wolf" src="img/BG_lightning.svg" />
       <img className="wolf" src="img/robot.svg" />
@@ -60,13 +60,13 @@ function SdkSection(props) {
         <h1>Learn more about the Overwolf SDK</h1>
         <ul>
           <SdkItem config={siteConfig} classNameImg="extra-padding-on-desktop" imgSrc="img/create.svg">
-            <a href={`${baseUrl}#`}>Easily create a desktop app with overlay capabilities</a>, using just HTML and JS. Your own desktop app, desktop icon, auto updates, anti-cheat compatibility and the works... we do it all for you
+            <a href={`${baseUrl}docs/start/sdk-introduction`}>Easily create a desktop app with overlay capabilities</a>, using just HTML and JS. Your own desktop app, desktop icon, auto updates, anti-cheat compatibility and the works... we do it all for you
           </SdkItem>
           <SdkItem className="reversed" config={siteConfig} imgSrc="img/events.svg">
-            <a href={`${baseUrl}#`}>Get access to real-time game events</a> for leading games and make your app as if it were integrated directly with the game itself
+            <a href={`${baseUrl}docs/api/overwolf-games-events`}>Get access to real-time game events</a> for leading games and make your app as if it were integrated directly with the game itself
           </SdkItem>
           <SdkItem config={siteConfig} classNameImg="extra-padding-on-desktop" imgSrc="img/monetize.svg">
-            <a href={`${baseUrl}#`}>Monetize your app</a> and make money by showing forgivable ads (= ads on loading screens, when gamers don’t have anything better to do anyway). It’s easier than implementing AdSense, not affected by ad blockers, not obtrusive and our team will optimize the experience with you
+            <a href={`${baseUrl}docs/start/monetization-overview`}>Monetize your app</a> and make money by showing forgivable ads (= ads on loading screens, when gamers don’t have anything better to do anyway). It’s easier than implementing AdSense, not affected by ad blockers, not obtrusive and our team will optimize the experience with you
           </SdkItem>
         </ul>
       </div>
@@ -99,7 +99,7 @@ function GameDevSection(props) {
           Community is power and a driver for retention.<br/>
           Give your community tools to create valuable in-game content for your game with the Overwolf SDK
         </p>
-        <a className="btn" href={`${baseUrl}#`}>Learn more</a>
+        <a className="btn" href={`${baseUrl}docs/topics/events-SDK-for-game-developers`}>Learn more</a>
       </div>
     </section>
   );
