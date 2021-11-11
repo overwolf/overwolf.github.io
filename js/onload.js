571458f2720d918170dd4ce467d396b0e9e8106a
eransharv <eransharv@users.noreply.github.com>
2021-11-11 14:46:57 +0000

    Deploy website
    Deploy website version based on 80ffa27fdd96651e0f438e54e206eb5249bebf0c

--- js/onload.js
+++ js/onload.js
@@ -3,7 +3,7 @@ function addSiderbarHighlight() {
   if (!selectedListItem) return;
   const listParent = selectedListItem.parents('ul').last().parent();
   const categoryTitle = listParent.children('.navGroupCategoryTitle').last();
-  categoryTitle.css('border-left', '4px solid #E5004C');
+  categoryTitle.addClass('is-active');
 }
 
 function moveEditButton() {
@@ -17,4 +17,59 @@ function moveEditButton() {
 window.onload = function () {
   addSiderbarHighlight();
   moveEditButton();
+  setNavScrollbarPosition();
+  window.addEventListener("resize", setNavScrollbarPosition);
+  addMobileButton();
+
+
+  let navButtons = document.querySelectorAll(".toc .toggleNav .navGroup .navGroupCategoryTitle");
+
+  navButtons.forEach(function(btn) {
+    btn.addEventListener("click", function(e) {
+      setNavScrollbarPosition();
+    });
+});
+
+
 };
+
+//position navbar scrollbar 
+
+function setNavScrollbarPosition() {
+  const NavScrollBar = document.querySelector('#docsNav > div.os-scrollbar.os-scrollbar-vertical > div');
+  if(NavScrollBar) {
+    let pageOffsetLeft = document.querySelector('.docMainWrapper.wrapper').offsetLeft;
+    let menuWidth = document.querySelector('#docsNav > div.os-padding > div').offsetWidth;
+    NavScrollBar.style.left = `${(pageOffsetLeft + menuWidth) + -1}px`;
+
+  }
+
+}
+
+function addMobileButton() {
+  const mobileBtnElement = `
+  <button id="hamburger-menu-button" class="hamburger-menu-button" onclick="openCloseMobileNavMenu()">
+    <div>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </button>`
+
+  document.querySelector('.fixedHeaderContainer').insertAdjacentHTML('afterbegin', mobileBtnElement);
+
+}
+
+function openCloseMobileNavMenu() {
+  console.log('click')
+  const navMenu = document.querySelector('.navigationSlider .slidingNav');
+  if(navMenu.classList.contains('is-open')){
+    navMenu.classList.remove('is-open');
+    document.querySelector('#hamburger-menu-button').classList.remove('close');
+    document.body.style.overflow = "";
+  } else {
+    navMenu.classList.add('is-open');
+    document.querySelector('#hamburger-menu-button').classList.add('close');
+    document.body.style.overflow = "hidden";
+  }
+}
