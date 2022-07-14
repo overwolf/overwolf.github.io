e9975c3b6450504868af7deab5838122fcceb030
Oded Itkin <oded.itkin@overwolf.com>
2022-07-14 14:06:35 +0300

    Various small fixes

---
+++ scripts/generators/changelogs.js
@@ -0,0 +1,18 @@
+const fs = require('fs') // filesystem
+const paths = ["../website/pages/docs/api/changelogs/"] // the paths where the code should run
+
+const logs = JSON.parse(fs.readFileSync("configs/changelogs.json", 'utf8'))
+// generator code
+
+fs.readFile("templates/changelogs.mdx", 'utf8', function (err, temp) {
+    if (err) {
+        return console.error(err)
+    }
+    let dev = logs["dev"]
+    let cur = logs["latest"]
+    fs.writeFile(paths[0] + "changelogs.mdx", temp.replaceAll("$latest-ver", cur["name"]).replaceAll("$latest-date", cur["date"]).replaceAll("$latest-path", cur["path"]).replaceAll("$dev-ver", dev["name"]).replaceAll("$dev-date", dev["date"]).replaceAll("$dev-path", dev["path"]), function (err) {
+        if (err) {
+            return console.error(err)
+        }
+    })
+})
