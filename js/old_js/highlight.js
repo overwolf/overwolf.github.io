54beeb24aa29eb7fea0b5bf646ab9dc326bf831f
Oded Itkin <OdedItkinOW@overwolf.com>
2022-07-14 14:04:28 +0000

    Deploy website - based on db0cececceea589030c076d26bf11441f75b92be

--- js/old_js/highlight.js
+++
@@ -1,3 +0,0 @@
-window.addEventListener('load', function() {
-  hljs.initHighlighting();
-});
--- js/old_js/luckyorange.js
+++
@@ -1,7 +0,0 @@
-window.__lo_site_id = 164110;
-(function() {
-    var wa = document.createElement('script'); 
-    wa.type = 'text/javascript'; wa.async = true;
-	wa.src = 'https://d10lpsik1i8c69.cloudfront.net/w.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(wa, s);
-})();
