f1003a2719456dcbd7984230316c6a6d4a3b58ed
Oded Itkin <oded.itkin@overwolf.com>
2022-09-18 18:50:27 +0300

    Fixed redirect issues

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -87,7 +87,7 @@ function applyOverrides(items) {
             if (item.frontMatter.sidebar_label != undefined) return
             item.frontMatter.sidebar_label = item.frontMatter.title
           }
-          if (rename = propExists(element, "rename")){
+          if (rename = propExists(element, "rename")) {
             var matcher = rename["matcher"]
             var replacer = rename["replacer"]
             item.frontMatter.sidebar_label = item.frontMatter.sidebar_label.replace(new RegExp(matcher), replacer)
@@ -127,23 +127,23 @@ function applyOverridePlasters(items, flattenCount, inverted, subsetReg, subsetR
             if (propExists(element, "reverseChildren")) {
               thisInvert = !inverted
             }
-            if (propExists(element, "sortChildren")){
+            if (propExists(element, "sortChildren")) {
               thisSort = true
             }
-            if(temp = propExists(element, "subset")){
+            if (temp = propExists(element, "subset")) {
               thisSubsetReg = temp["subsetRegex"]
               thisSubsetRename = temp["subsetRename"]
             }
           }
         }
       });
-      if(thisSubsetReg && thisSubsetRename){
+      if (thisSubsetReg && thisSubsetRename) {
         item.label = item.label.replaceAll(new RegExp(thisSubsetReg, 'g'), thisSubsetRename)
       }
       if (item.items.length === 0) result.push(item.link)
       else if (thisFlatten < 0) {
         applyOverridePlasters(item.items, thisFlatten, thisInvert, thisSubsetReg, thisSubsetRename, thisSort).forEach((item) => result.push(item))
-        if(sort){
+        if (sort) {
           result = result.sort((a, b) => a.label.localeCompare(b.label))
         }
       } else {
@@ -172,130 +172,130 @@ function propExists(object, prop) {
 const codeComponentTagger = require("./src/plugins/tagging/codeComponentTagger.js").default;
 
 /** @type {import('@docusaurus/types').Config} */
-async function config(){
-  
+async function config() {
+
   return {
-  title: "Overwolf",
-  tagline: "Easily create apps for PC games on the Overwolf framework",
-  url: "https://overwolf.github.io",
-  baseUrl: "/",
-  trailingSlash: false,
-  organizationName: "overwolf",
-  projectName: "overwolf.github.io",
-  deploymentBranch: 'gh-pages',
-  scripts: [
-    
-  ],
-  stylesheets: [
-    "https://fonts.googleapis.com/css?family=Lato%3A400%2C400i%2C700%2C900&ver=4.5.3",
-  ],
-  favicon: "/img/ow-favicon.ico",
-  customFields: {
-    wrapPagesHTML: true,
-    homepage_title: "The Developers Site",
-    markdownPlugins: [
-      null
+    title: "Overwolf",
+    tagline: "Easily create apps for PC games on the Overwolf framework",
+    url: "https://overwolf.github.io",
+    baseUrl: "/",
+    trailingSlash: false,
+    organizationName: "overwolf",
+    projectName: "overwolf.github.io",
+    deploymentBranch: 'gh-pages',
+    scripts: [
+
     ],
-    users: [
-      {
-        caption: "User1",
-        image: "/img/overwolf-logo_300x240.svg",
-        infoLink: "https://www.overwolf.com",
-        pinned: true
-      }
+    stylesheets: [
+      "https://fonts.googleapis.com/css?family=Lato%3A400%2C400i%2C700%2C900&ver=4.5.3",
     ],
-    separateCss: [
-      "/html/submission-form/style.css",
-      "/html/submission-form/select2.css"
-    ]
-  },
-  onBrokenLinks: "log",
-  onBrokenMarkdownLinks: "log",
-  plugins: ['docusaurus-plugin-sass', 'docusaurus-plugin-clarity', ['@docusaurus/plugin-client-redirects',
-    {
-      redirects: require('./hierarchies/redirects.json')
-    }],
-    [
-      require.resolve('docusaurus-gtm-plugin'),
+    favicon: "/img/ow-favicon.ico",
+    customFields: {
+      wrapPagesHTML: true,
+      homepage_title: "The Developers Site",
+      markdownPlugins: [
+        null
+      ],
+      users: [
+        {
+          caption: "User1",
+          image: "/img/overwolf-logo_300x240.svg",
+          infoLink: "https://www.overwolf.com",
+          pinned: true
+        }
+      ],
+      separateCss: [
+        "/html/submission-form/style.css",
+        "/html/submission-form/select2.css"
+      ]
+    },
+    onBrokenLinks: "log",
+    onBrokenMarkdownLinks: "log",
+    plugins: ['docusaurus-plugin-sass', 'docusaurus-plugin-clarity', ['@docusaurus/plugin-client-redirects',
       {
-        id: 'GTM-NQD72PT',
-      }
-    ]],
-  presets: [
-    [
-      "@docusaurus/preset-classic",
-      {
-        docs: {
-          remarkPlugins: [codeComponentTagger],
-          showLastUpdateAuthor: true,
-          showLastUpdateTime: true,
-          editUrl: "https://github.com/overwolf/overwolf.github.io/tree/source/website/",
-          path: "pages/docs",
-          routeBasePath: "/",
-          sidebarPath: require.resolve("./hierarchies/sidebars.js"),
-          sidebarItemsGenerator: sidebarsOverrides,
-          sidebarCollapsible: true,
-          sidebarCollapsed: true
+        redirects: require('./hierarchies/redirects.json')
+      }],
+      [
+        require.resolve('docusaurus-gtm-plugin'),
+        {
+          id: 'GTM-NQD72PT',
+        }
+      ]],
+    presets: [
+      [
+        "@docusaurus/preset-classic",
+        {
+          docs: {
+            remarkPlugins: [codeComponentTagger],
+            showLastUpdateAuthor: true,
+            showLastUpdateTime: true,
+            editUrl: "https://github.com/overwolf/overwolf.github.io/tree/source/website/",
+            path: "pages/docs",
+            routeBasePath: "/",
+            sidebarPath: require.resolve("./hierarchies/sidebars.js"),
+            sidebarItemsGenerator: sidebarsOverrides,
+            sidebarCollapsible: true,
+            sidebarCollapsed: true
+          },
+          blog: {},
+          theme: {
+            customCss: [require.resolve('./src/css/style.scss')],
+          },
+          googleAnalytics: {
+            trackingID: "UA-144253676-1"
+          },
         },
-        blog: {},
-        theme: {
-          customCss: [require.resolve('./src/css/style.scss')],
-        },
-        googleAnalytics: {
-          trackingID: "UA-144253676-1"
-        },
+      ]
+    ],
+    themeConfig: {
+      docs: {
+        sidebar: {
+          autoCollapseCategories: true
+        }
       },
-    ]
-  ],
-  themeConfig: {
-    docs: {
-      sidebar: {
-        autoCollapseCategories: true
-      }
-    },
-    metadata: [
-      {
-        name: "google-site-verification",
-        content: "EWs1HNaDLCjSSyCLv5WSFYAd1H2JU-yXjFjesu5nW8g"
-      }
-    ],
-    clarity: {
-      ID: "btoodir3td",
-    },
-    colorMode: {
-      defaultMode: 'dark',
-      disableSwitch: false,
-      respectPrefersColorScheme: false,
-    },
-    navbar: {
-      style: "dark",
-      title: "Overwolf",
-      logo: {
-        src: "img/header-icon.svg"
+      metadata: [
+        {
+          name: "google-site-verification",
+          content: "EWs1HNaDLCjSSyCLv5WSFYAd1H2JU-yXjFjesu5nW8g"
+        }
+      ],
+      clarity: {
+        ID: "btoodir3td",
       },
-      items: require('./hierarchies/headers.json')
-    },
-    image: "img/embed/dev.jpg",
-    footer: {
-      style: "dark",
-      links: require('./hierarchies/footer.json'),
-      copyright: "<div class='c-inner'><p>Copyright © 2022 Overwolf</p></div>",
-      logo: {
-        src: "/img/ow-footer.svg"
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false,
+        respectPrefersColorScheme: false,
+      },
+      navbar: {
+        style: "dark",
+        title: "Overwolf",
+        logo: {
+          src: "img/header-icon.svg"
+        },
+        items: require('./hierarchies/headers.json')
+      },
+      image: "img/embed/dev.jpg",
+      footer: {
+        style: "dark",
+        links: require('./hierarchies/footer.json'),
+        copyright: "<div class='c-inner'><p>Copyright © 2022 Overwolf</p></div>",
+        logo: {
+          src: "/img/ow-footer.svg"
+        }
+      },
+      prism: {
+        additionalLanguages: ['powershell'],
+      },
+      algolia: {
+        apiKey: 'cc7909d516aa0fbd71371ad4b3769c7e',
+        indexName: 'overwolf',
+        appId: '4YSGJ6Z1HG',
+        contextualSearch: true,
+        algoliaOptions: {}
       }
-    },
-    prism: {
-      additionalLanguages: ['powershell'],
-    },
-    algolia: {
-      apiKey: 'cc7909d516aa0fbd71371ad4b3769c7e',
-      indexName: 'overwolf',
-      appId: '4YSGJ6Z1HG',
-      contextualSearch: true,
-      algoliaOptions: {}
     }
   }
-}
 }
 
 module.exports = config();
