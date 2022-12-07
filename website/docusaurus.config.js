a9ca7c58693a9441af389c9ac8f88c409b7b7566
Oded Itkin <oded.itkin@overwolf.com>
2022-12-07 10:22:29 +0200

    Ads sdk docs progress, and general cleanup

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,189 +1,50 @@
 // loads the sidebars override file
 
 const sidebarOverrides = require("./hierarchies/sidebaroverrides.json");
-const path = require('path');
-console.log(path.resolve(__dirname, "pages/docs"));
-/* this file currently supports the following schema:
-
-{
-  // sidebars argument manipulation
-  "overrides": {
-    // category manipulation
-    "categories": [
-      {
-        // category identifier, matching on the item.unversionedId, aka the raw path link
-        "id": regex,
-        // when used, renames items with a certain rule, using .replace
-        "rename"?: {
-          // regex used to match with the item's label. can utilize capture groups to avoid erasing parts of the pattern
-          "matcher": regex,
-          // the replace regex used to replace the items' labels
-          "replacer": regexReplace "$1",
-        }
-        // whether or not the sidebar label should be inferred from the page title. if implicit is marked, and the sidebar has an explicit title, this returns early
-        "implicit"?: bool,
-        // whether or not the sidebar's title should be automatically capitalized for every word
-        "capitalize"?: bool,
-        // defines the length of a sidebar title that will be inferred to be an acronym. sidebar titles with this length or less are considered acronyms, and are forcefully fully capitalized
-        "acronymSize"?: number,
-        // defines the default sidebar position for any item that doesn't have one defined
-        "defaultOrder"?: number
-        // defined whether or not items matched with this `id` should have their sidebar position reversed or not
-        "reverse"?: bool
-        }
-    ]
-  }
-  // generated sidebars manipulation
-  "plasters": {
-    // category manipulation
-    "categories": [
-      {
-        // category identifier, matching on the item.link.id property, aka the link this category points to
-        "id": regex,
-        // how many layers of categories should be skipped before flattening all children. a value of 1 corresponds to item[subItem1[children...], subItem2[children...]]
-        "flattenChildren"?: number,
-        // should the children of this element be reversed (recursive per category). **has weird artifacts when used with flatten children, enable `sortChildren` to fix those
-        "reverseChildren"?: bool,
-        // should the flattened children be sorted forcefully after all their individual code is ran. sorts alphabetically by label
-        "sortChildren"?: bool,
-        // when used, forcefully renames sub-items with a certain rule, using .replaceAll
-        "subset"?: 
-        {
-          // regex used to match with the item's label. runs recursively on all items contained by this category. can utilize capture groups to avoid erasing parts of the pattern
-          "subsetRegex": regex,
-          // the replace regex used to replace the items' labels
-          "subsetRename": regexReplace
-        }
-      }
-    ]
-  }
-}
-
-*/
 
 // @ts-check
 
-function applyOverrides(items) {
-  return items.map((item) => {
-    sidebarOverrides["overrides"]["categories"].forEach(element => {
-      if (id = propExists(element, "id")) {
-        if (item.unversionedId.match(new RegExp(id))) {
-          if (propExists(element, "flattenChildren")) {
-            childID = id + `/`
-            if (item.unversionedId.match(new RegExp(childID))) {
-              item.frontMatter.sidebar_label += "[FLATTEN]"
-            }
-          }
-          if ((defaultOrder = propExists(element, "defaultOrder")) === 0) {
-            if (item.frontMatter.sidebar_position === undefined) {
-              item.frontMatter.sidebar_position = defaultOrder
-            }
-          }
-          if (propExists(element, "reverse")) {
-            item.frontMatter.sidebar_position = -item.frontMatter.sidebar_position;
-          }
-          item.sidebarPosition = item.frontMatter.sidebar_position
-
-          if(propExists(item.frontMatter, "overwolf_platform") && !item.frontMatter.sidebar_label.match(/\[OW\]/)){
-            item.frontMatter.sidebar_label += "[OW]"
-          }
-
-          if (propExists(element, "implicit")) {
-            if (item.frontMatter.sidebar_label != undefined) return
-            item.frontMatter.sidebar_label = item.frontMatter.title
-          }
-          if (rename = propExists(element, "rename") && item.frontMatter.sidebar_label) {
-            var matcher = rename["matcher"]
-            var replacer = rename["replacer"]
-            item.frontMatter.sidebar_label = item.frontMatter.sidebar_label.replace(new RegExp(matcher), replacer)
-          }
-          if (size = propExists(element, "acronymSize") && item.frontMatter.sidebar_label) {
-            if (item.frontMatter.sidebar_label.length <= size) {
-              item.frontMatter.sidebar_label = item.frontMatter.sidebar_label.toUpperCase()
-            }
-          }
-          if (propExists(element, "capitalize") && item.frontMatter.sidebar_label) {
-            item.frontMatter.sidebar_label = item.frontMatter.sidebar_label.split(/[ .-]/gm).map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(" ")
-          }
-        }
-      }
-    });
-    return item;
-  })
-}
-
-function classNamer(customProps){
+function classNamer(curName, customProps){
   var result = []
+  if(curName) result.push(curName);
   if(customProps.overwolf_platform) result.push("platform")
   if(customProps.electron_platform) result.push("electron")
   return result.join(" ");
 }
 
-function applyOverridePlasters(items, flattenCount, inverted, subsetReg, subsetRename, sort) {
+function applyCustomSidebarProps(items) {
   var result = []
-  if (flattenCount != undefined) flattenCount--
   items.forEach(item => {
-    var thisInvert = inverted
-    var thisFlatten = flattenCount
-    var thisSubsetReg = subsetReg
-    var thisSubsetRename = subsetRename
-    var thisSort = sort
-    if(propExists(item, "customProps")){
-      var className = classNamer(item.customProps)
-      if(className != "")
-      {
-        if(item.className) className = `${item.className} ${className}`
-        item.className = className;
-      } 
+    var hideLayout = false;
+    var reverse = false;
+    var customProps = undefined;
+    if(customProps = propExists(item, "customProps")){
+      hideLayout = propExists(customProps, "skip_layout");
+      reverse = propExists(customProps, "reverse_items");
+      // var className = classNamer(item.customProps)
+      // if(className != "")
+      // {
+      //   if(item.className) className = `${item.className} ${className}`
+      //   item.className = className;
+      // } 
     }
     if (item.type === "category") {
-      sidebarOverrides["plasters"]["categories"].forEach(element => {
-        if (id = propExists(element, "id")) {
-          if (item.link && item.link.id && item.link.id.match(new RegExp(id))) {
-            if ((layers = propExists(element, "flattenChildren")) != undefined) {
-              thisFlatten = layers
-            }
-            if (propExists(element, "reverseChildren")) {
-              thisInvert = !inverted
-            }
-            if (propExists(element, "sortChildren")) {
-              thisSort = true
-            }
-            if (temp = propExists(element, "subset")) {
-              thisSubsetReg = temp["subsetRegex"]
-              thisSubsetRename = temp["subsetRename"]
-            }
-          }
-        }
-      });
-      if (thisSubsetReg && thisSubsetRename) {
-        item.label = item.label.replaceAll(new RegExp(thisSubsetReg, 'g'), thisSubsetRename)
-      }
-      if (item.items.length === 0) result.push(item.link)
-      else if (thisFlatten < 0) {
-        applyOverridePlasters(item.items, thisFlatten, thisInvert, thisSubsetReg, thisSubsetRename, thisSort).forEach((item) => result.push(item))
-        if (sort) {
-          result = result.sort((a, b) => a.label.localeCompare(b.label))
-        }
+      // if (item.items.length === 0 && item.link) result.push(item.link)
+      if (hideLayout) {
+        applyCustomSidebarProps(item.items).forEach((item) => result.push(item))
       } else {
-        result.push({ ...item, items: applyOverridePlasters(item.items, thisFlatten, thisInvert, thisSubsetReg, thisSubsetRename, thisSort) })
+        var mutatedItem = { ...item, items: applyCustomSidebarProps(item.items) };
+        if(reverse) mutatedItem.items = mutatedItem.items.reverse();
+        result.push(mutatedItem)
       }
     } else result.push(item)
   })
-  return inverted ? result.reverse() : result
+  return result;
 }
 
 async function sidebarsOverrides({ defaultSidebarItemsGenerator, ...args }) {
-  // if (item = propExists(args, "item")) {
-  //   if ((dirName = propExists(item, "dirName")) && (type = propExists(item, "type")) && (type === "autogenerated")) {
-  //     if (docs = propExists(args, "docs")) {
-  //       args["docs"] = applyOverrides(docs, dirName);
-  //     }
-  //   }
-  // }
-  let x = await defaultSidebarItemsGenerator(args);
-  return x;
-  // return applyOverridePlasters(x, undefined, false, undefined, undefined, false)
+  let items = await defaultSidebarItemsGenerator(args);
+  return applyCustomSidebarProps(items);
 }
 
 function propExists(object, prop) {
