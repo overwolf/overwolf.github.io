eb4f6374d1c66e66d6d9a1528893c8e0d2e64bef
benjokoren-ow <ben.koren@overwolf.com>
2023-01-04 11:54:36 +0200

    start of services section

--- website/src/components/home-page/services/services.jsx
+++ website/src/components/home-page/services/services.jsx
@@ -8,7 +8,59 @@ const Services = (props) => {
 
   return (
     <section className="services-section">
-      <p>services</p>
+      <ul className='services-inner'>
+        <li className='service-item'>
+          <div className='assets'>
+            <img src='/img/home-2023/platform-app-bg.webp' alt='Overwolf Platform App'/>
+            <img src='/img/home-2023/platform-app.webp' alt='Overwolf Platform App'  />
+          </div>
+          <div className='info'>
+            <h2>Overwolf Platform App</h2>
+            <p>Create powerful desktop apps and overlays for AAA games, using basic HTML and Javascript</p>
+            <a href='/start/getting-started' className='btn-primary'>Explore</a>
+            <ul className='app-info-list'>
+              <li>
+                <svg><use href="/img/sprite.svg#monetization" /></svg>
+                Monetization
+              </li>
+              <li>
+                <svg><use href="/img/sprite.svg#in-game-overlay" /></svg>
+                In-game overlay
+              </li>
+              <li>
+                <svg><use href="/img/sprite.svg#real-time-game-events" /></svg>
+                Real time game events
+              </li>
+            </ul>
+          </div>
+        </li>
+
+        <li className='service-item'>
+          <div className='assets'>
+            <img src='/img/home-2023/electron-app-bg.webp' alt='Overwolf Electron App' />
+            <img src='/img/home-2023/electron-app.webp' alt='Overwolf Electron App' />
+          </div>
+          <div className='info'>
+            <h2>Overwolf Electron App</h2>
+            <p>Build cross-platform desktop apps with JavaScript, HTML, and CSS</p>
+            <a href='/tools/ow-electron' className='btn-primary'>Explore</a>
+            <ul className='app-info-list'>
+              <li>
+                <svg><use href="/img/sprite.svg#monetization" /></svg>
+                Monetization
+              </li>
+              <li>
+                <svg><use href="/img/sprite.svg#in-game-overlay" /></svg>
+                In-game overlay <span>(coming soon)</span>
+              </li>
+              <li>
+                <svg><use href="/img/sprite.svg#real-time-game-events" /></svg>
+                Real time game events <span>(coming soon)</span>
+              </li>
+            </ul>
+          </div>
+        </li>
+      </ul>
     </section>
   );
 
