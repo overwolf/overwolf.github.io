6a28b22ca6a0c9a06cd35cbeb96ec5301f2df4ed
benjokoren-ow <ben.koren@overwolf.com>
2023-01-08 13:41:04 +0200

    animate image on button mouse hover in service section

--- website/src/components/home-page/services/services.jsx
+++ website/src/components/home-page/services/services.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import { useState } from 'react';
 import './services.scss';
 
 const Services = (props) => {
  const {} = props;
 
-// ---------------------------------------------------------------------------
+ const [appBtn, seAppBtn] = useState(false);
+ const [electronAppBtn, seElectronAppBtn] = useState(false);
+
+// --------------------------------------------------------------------------- 
+
+  const handleToggleApp = () => {
+    seAppBtn(current => !current);
+  }
+
+  const handleToggleElectronApp = () => {
+    seElectronAppBtn(current => !current);
+  }
 
   return (
     <section className="services-section">
       <ul className='services-inner'>
-        <li className='service-item'>
+        <li className={`service-item ${appBtn ? 'btn-hover' : ''}`}>
           <div className='assets'>
             <img src='/img/home-2023/platform-app-bg.webp' alt='Overwolf Platform App' className='bg'/>
             <img src='/img/home-2023/platform-app.webp' alt='Overwolf Platform App' className='platform-app symbol' />
@@ -17,7 +29,11 @@ const Services = (props) => {
           <div className='info'>
             <h2>Overwolf Platform App</h2>
             <p>Create powerful desktop apps and overlays for AAA games, using basic HTML and Javascript</p>
-            <a href='/start/getting-started' className='btn-primary'>Explore</a>
+            <a href='/start/getting-started' 
+              className='btn-primary' 
+              onMouseEnter={handleToggleApp}
+              onMouseLeave={handleToggleApp}>
+              Explore</a>
             <ul className='app-info-list'>
               <li>
                 <svg><use href="/img/sprite.svg#monetization" /></svg>
@@ -35,7 +51,7 @@ const Services = (props) => {
           </div>
         </li>
 
-        <li className='service-item'>
+        <li className={`service-item ${electronAppBtn ? 'btn-hover' : ''}`}>
           <div className='assets'>
             <img src='/img/home-2023/electron-app-bg.webp' alt='Overwolf Electron App' className='bg' />
             <img src='/img/home-2023/electron-app.webp' alt='Overwolf Electron App' className='electron-app symbol' />
@@ -43,7 +59,11 @@ const Services = (props) => {
           <div className='info'>
             <h2>Overwolf Electron App</h2>
             <p>Build cross-platform desktop apps with JavaScript, HTML, and CSS</p>
-            <a href='/tools/ow-electron' className='btn-primary'>Explore</a>
+            <a href='/tools/ow-electron'            
+              className='btn-primary' 
+              onMouseEnter={handleToggleElectronApp}
+              onMouseLeave={handleToggleElectronApp}>
+              Explore</a>
             <ul className='app-info-list'>
               <li>
                 <svg><use href="/img/sprite.svg#monetization" /></svg>
