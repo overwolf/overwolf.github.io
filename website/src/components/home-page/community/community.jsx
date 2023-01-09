0109bdb89999146c8753ef595297e0b9288b779d
benjokoren-ow <ben.koren@overwolf.com>
2023-01-09 11:55:50 +0200

    play video on hover

--- website/src/components/home-page/community/community.jsx
+++ website/src/components/home-page/community/community.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import { useRef, useEffect } from 'react';
 import './community.scss';
 
 const Community = (props) => {
  const {} = props;
+ const signVideo = useRef(null);
+ const compassVideo = useRef(null);
+  // ---------------------------------------------------------------------------
 
-// ---------------------------------------------------------------------------
+  useEffect(() => {
+    //pause video onload because its has autoplay which a must to load first frame of image in the video
+    signVideo.current.pause();
+    compassVideo.current.pause();
+
+  }, []);
+
+  const handlePlaySignVideo = () => {
+    signVideo.current.play();
+  }
+
+  const handlePlayCompassVideo = () => {
+    compassVideo.current.play();
+  }
 
   return (
     <section className="community-section">
-      <div className='discord-community'>
+      <div className='discord-community' onMouseEnter={handlePlaySignVideo}>
         <div className='info'>
           <svg><use href="/img/sprite.svg#discord-big" /></svg>
           <h4>Join our community</h4>
@@ -16,13 +33,13 @@ const Community = (props) => {
           <a href='https://discord.com/invite/overwolf-developers' target={"_blank"} className='btn-secondary'>Join</a>
         </div>
         <div className='assets'>
-          <video className="sign-video" autoPlay loop muted playsInline poster="/img/home-2023/community-poster.webp">
+          <video className="sign-video" autoPlay loop muted playsInline ref={signVideo}>
             <source src="/img/home-2023/video/sign.webm" type="video/webm" />
           </video>
         </div>
       </div>
 
-      <div className='game-developer'>
+      <div className='game-developer' onMouseEnter={handlePlayCompassVideo}>
 
         <div className='info'>
           <h4>Game developer?</h4>
@@ -32,7 +49,7 @@ Give your community tools to create valuable in-game content for your game with 
         </div>
 
         <div className='assets'>
-          <video className="compass-video" autoPlay loop muted playsInline poster="/img/home-2023/compass-poster.webp">
+          <video className="compass-video" autoPlay loop muted playsInline ref={compassVideo}>
             <source src="/img/home-2023/video/compass.webm" type="video/webm" />
           </video>
         </div>
