3442e85e76fa72bc852a796de0b0b9b6c597c39d
benjokoren-ow <ben.koren@overwolf.com>
2023-01-09 13:52:34 +0200

    jumping on load bug

--- website/src/components/home-page/services/services.jsx
+++ website/src/components/home-page/services/services.jsx
@@ -23,7 +23,11 @@ const Services = (props) => {
       <ul className='services-inner'>
         <li className={`service-item ${appBtn ? 'btn-hover' : ''}`}>
           <div className='assets'>
-            <img src='/img/home-2023/platform-app-bg.webp' alt='Overwolf Platform App' className='platform-app-bg bg'/>
+            <img
+              src='/img/home-2023/platform-app-bg.webp'
+              alt='Overwolf Platform App'
+              className='platform-app-bg bg'
+              width={512} height={280} />
             <img src='/img/home-2023/platform-app.webp' alt='Overwolf Platform App' className='platform-app symbol' />
           </div>
           <div className='info'>
@@ -53,7 +57,11 @@ const Services = (props) => {
 
         <li className={`service-item ${electronAppBtn ? 'btn-hover' : ''}`}>
           <div className='assets'>
-            <img src='/img/home-2023/electron-app-bg.webp' alt='Overwolf Electron App' className='electron-app-bg bg' />
+            <img
+              src='/img/home-2023/electron-app-bg.webp'
+              alt='Overwolf Electron App'
+              className='electron-app-bg bg'
+              width={512} height={280} />
             <img src='/img/home-2023/electron-app.webp' alt='Overwolf Electron App' className='electron-app symbol' />
           </div>
           <div className='info'>
