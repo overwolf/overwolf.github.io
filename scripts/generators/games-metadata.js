bd812c365feb30db43a3c9c3d4dcc7618489e182
OdedItkinOW <oded.itkin@overwolf.com>
2023-01-11 12:25:47 +0200

    Changed metadata to work with json

--- scripts/generators/games-metadata.js
+++ scripts/generators/games-metadata.js
@@ -1,69 +1,63 @@
 const fs = require('fs') // filesystem
-const dec = require("./declare");
+const { declare } = require("./declare");
 const paths = ["../website/static/js/games_metadata.js", "../website/src/components/game-events-status/gamesMetaData.jsx"] // the paths where the code should run
 const compliance = "../website/pages/docs/start/game-compliance/"
 
 // generator code
 
-fs.readFile("configs/games-metadata.json", 'utf8', function(err, data) {
+function getIfExists(val, path) {
+    return val[path]
+}
+
+fs.readFile("configs/games-metadata.json", 'utf8', function (err, data) {
     if (err) {
         return console.error(err)
     }
     const json = JSON.parse(data)
-    fs.readFile("templates/metadata.cfg", 'utf8', function(err, temp) {
+    const GamesMetaData = {}
+
+    json.forEach(function (val) {
+        let id = val["id"]
+        let path = val["path"]
+        let name = val["name"]
+
+        let games = getIfExists(val, "games")
+        let compliant = fs.existsSync(compliance + path + ".mdx")
+        let isLauncher = games != undefined
+
+        let fullPath = `${isLauncher ? 'launchers/' : ''}${path}`
+
+        GamesMetaData[id] = {
+            id,
+            displayId: getIfExists(val, "displayId"),
+            name,
+            path: `/status/${fullPath}`,
+            launcherID: getIfExists(val, "launcher"),
+            mainVariant: getIfExists(val, "mainVariant"),
+            subVariant: getIfExists(val, "subVariant"),
+            compliance: compliant ? `/start/game-compliance/${path}` : undefined,
+            docs: `/api/games/${isLauncher ? 'launchers/' : ''}events/${path}`,
+            iconUrl: `/img/games-logos/${fullPath}.png`,
+            iconLargeUrl: `/img/games-logos/large/${fullPath}.png`,
+            launcher: isLauncher ? { games } : undefined
+        }
+    })
+    const stringified = JSON.stringify(GamesMetaData, undefined, 4);
+
+    let oldJSON = `const GamesMetaData = ${stringified}\n\nconsole.log('GamesMetaData is loaded locally');`
+
+    let newJSON = `export const GamesMetadata = ${stringified}`
+
+    fs.writeFile(paths[0], oldJSON, 'utf8', function (err) {
         if (err) {
             return console.error(err)
         }
-        let first = "const GamesMetaData = {"
-        let cont = []
-        let second = "export const GamesMetadata = {"
-
-        json.forEach(function(val) {
-            let id = val["id"]
-            let path = val["path"]
-            let name = val["name"]
-            let displayId = val.hasOwnProperty("displayId") ? val["displayId"] : -1
-            let launcher = val.hasOwnProperty("launcher") ? val["launcher"] : -1
-            let games = val.hasOwnProperty("games") ? val["games"] : undefined
-            let mainVariant = val.hasOwnProperty("mainVariant") ? val["mainVariant"] : -1
-            let subVariant = val.hasOwnProperty("subVariant") ? val["subVariant"] : -1
-
-            let compliant = fs.existsSync(compliance + path + ".mdx")
-            let isLauncher = games != undefined
-
-            cont.push(temp
-            .replaceAll("$id", id)
-            .replaceAll("$path", path)
-            .replaceAll(/\$display\((.*?)\)/gms, displayId != -1 ? "$1" : "")
-            .replaceAll("$displayId", displayId)
-            .replaceAll("$name", name)
-            .replaceAll(/\$compliant\((.*?)\)/gms, compliant ? "$1" : "")
-            .replaceAll(/\$launchers\((.*?)\)/gms, isLauncher ? "$1" : "")
-            .replaceAll("$games", games)
-            .replaceAll(/\$launched\((.*?)\)/gms, launcher != -1 ? "$1" : "")
-            .replaceAll("$launcherID", launcher)
-            .replaceAll(/\$mvar\((.*?)\)/gms, mainVariant != -1 ? "$1" : "")
-            .replaceAll("$mvariant", mainVariant)
-            .replaceAll(/\$svar\((.*?)\)/gms, subVariant != -1 ? "$1" : "")
-            .replaceAll("$svariant", subVariant)
-            )
-        })
-        let contS = cont.join(",")
-        first += contS
-        first += "\n}\n\nconsole.log('GamesMetadata is loaded locally')"
-        second += contS
-        second += "\n}"
-        fs.writeFile(paths[0], first, 'utf8', function(err) {
-            if (err) {
-                return console.error(err)
-            }
-            dec.declare(paths[0]);
-        })
-        fs.writeFile(paths[1], second, 'utf8', function(err) {
-            if (err) {
-                return console.error(err)
-            }
-            dec.declare(paths[1])
-        })
+        declare(paths[0]);
+    })
+    fs.writeFile(paths[1], newJSON, 'utf8', function (err) {
+        if (err) {
+            return console.error(err)
+        }
+        declare(paths[1])
     })
 })
