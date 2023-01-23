c55af8df3b69379f7984254fceaa9bbc5642d891
benjokoren-ow <ben.koren@overwolf.com>
2023-01-23 13:46:37 +0200

    mdx

--- website/src/theme/MDXComponents.js
+++ website/src/theme/MDXComponents.js
@@ -6,11 +6,11 @@ import ImageBoxModal from '@site/src/components/image-box-modal/image-box-modal'
 import VideoBox from '@site/src/components/video-box/video-box';
 import GameInfo from '@site/src/components/game-info/game-info';
 import SkipMe from '@site/src/components/skip-me/skip-me';
+import OWTabs from '@site/src/components/ow-tabs/ow-tabs';
 import Tagger from '../components/tagging/Tagger';
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
 import SimpleTab from '../components/simple-tab/simple-tab';
-
 
 export default {
   // Re-use the default mapping
@@ -22,6 +22,7 @@ export default {
   VideoBox: VideoBox,
   GameInfo: GameInfo,
   SkipMe: SkipMe,
+  OWTabs: OWTabs,
   Tagger: Tagger,
   Tabs: Tabs,
   TabItem: TabItem,
