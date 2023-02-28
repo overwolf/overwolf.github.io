7e76f5d81a951fa4935c6d434039321b548a61cb
benjokoren-ow <ben.koren@overwolf.com>
2023-02-28 17:45:31 +0200

    tabs

--- website/src/components/ow-table/ow-table-row.tsx
+++ website/src/components/ow-table/ow-table-row.tsx
@@ -1,5 +1,5 @@
 // import './ow-tabs.scss';
-import React, {FC, Children} from 'react';
+import React, {FC, Children, useRef} from 'react';
 
 interface OWTableRowProps {
   children: React.ReactNode;
@@ -14,11 +14,26 @@ const OWTableRow: FC<OWTableRowProps> = props => {
     isExpended = false
   } = props;
 
+  const row = useRef<HTMLInputElement>(null);
+
+  const handleExpandRow = () => {
+    if(row.current !== null) {
+      const expandedRow = row.current.nextElementSibling as HTMLElement;;
+      const expandedRowContent = expandedRow?.firstChild as HTMLElement;
+      const expandedRowHeight = expandedRowContent?.offsetHeight;
+
+      expandedRow.style.display = "table-row";
+      expandedRow.style.height = `${expandedRowHeight}px`;
+
+    }
+
+  }
+
   return (
-    <div className='ow-table-body-row'>
+    <div className='ow-table-body-row' ref={row}>
       {children}
       {isExpended &&
-      <button className='expand-row' title="expand row">
+      <button className='expand-row' title="expand row" onClick={handleExpandRow}>
         <span className={`hidden`}>Hide</span>
         <svg><use href="/img/sprite.svg#arrowDown" /></svg>
       </button>
