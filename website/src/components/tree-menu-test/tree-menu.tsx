9ce78b1e9d365e43d249476ab55c752738e20aa0
benjokoren-ow <ben.koren@overwolf.com>
2023-04-30 08:08:38 +0300

    delete test files

---
+++ website/src/components/tree-menu-test/tree-menu.tsx
@@ -0,0 +1,74 @@
+import './tree-menu.scss';
+import React, {FC, Children} from 'react';
+
+interface TreeMenuProps {
+  children: React.ReactNode;
+  codeBlockData: string;
+}
+
+// -----------------------------------------------------------------------------
+
+const TreeMenu: FC<TreeMenuProps> = props => {
+  const {
+    children,
+    codeBlockData
+  } = props;
+
+  // -----------------------------------------------------------------------------
+
+  return (
+    <section className='tree-menu-block'>
+      <span className='type'>Start{`(`}</span>
+      <ul className='tree-menu'>
+        <li className='is-open'>
+          {/* <svg><use href="/img/sprite.svg#arrowDown"></use></svg> */}
+          <div className='property'>
+            <span className='key'>Settings:</span>
+            <button className='expand-btn'>{`{...}`}</button>
+          </div>
+          <ul className='tree-menu-sub-level-1'>
+            <li>
+              <div className='property'>
+                <span className='key'>buffer_length</span>
+                <span className='value'>20000</span>
+              </div>
+            </li>
+          </ul>
+        </li>
+        <li>
+          {/* <svg><use href="/img/sprite.svg#arrowDown"></use></svg> */}
+          <div className='property'>
+            <span className='key'>Sound:</span>
+            <button className='expand-btn'>{`{...}`}</button>
+          </div>
+          <ul className='tree-menu-sub-level-1'>
+            <li>
+              <div className='property'>
+                <span className='key'>buffer_length</span>
+                <span className='value'>20000</span>
+              </div>
+            </li>
+          </ul>
+        </li>
+        <li>
+          {/* <svg><use href="/img/sprite.svg#arrowDown"></use></svg> */}
+          <div className='property'>
+            <span className='key'>Sound:</span>
+            <button className='expand-btn'>{`{...}`}</button>
+          </div>
+          <ul className='tree-menu-sub-level-1'>
+            <li>
+              <div className='property'>
+                <span className='key'>buffer_length</span>
+                <span className='value'>20000</span>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <span className='type'>{`)`}</span>
+    </section>
+  );
+};
+
+export default TreeMenu;
