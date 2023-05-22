2e529a8e53e5aafcb467430f18fb8cd74746372f
OdedItkinOW <oded.itkin@overwolf.com>
2023-05-22 13:31:20 +0300

    Small cleanup

--- website/src/components/skip-me/skip-me.tsx
+++ website/src/components/skip-me/skip-me.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import * as ReactDOMServer from 'react-dom/server';
+import React, { useEffect } from 'react';
 import useDebugInfoHook from '../debug/debug-info-hook';
 import useDevelopmentModeHook from '../hooks/development-mode-hook';
 
 
 function SkipMe(props: { children: any; type: any; }) {
   var { type, children } = props;
+
   const devMode = useDevelopmentModeHook();
   //  if(!type) console.error("Missing skipMe type!");
   if (!devMode) return (<></>);
   const [debug, setDebug] = useDebugInfoHook();
 
-  console.log(children);
   return (
     <div style={{ color: "grey", paddingLeft: "10px" }}>
       {
