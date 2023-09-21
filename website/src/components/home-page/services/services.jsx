bb1a1248fb35ce40cb75ad7054419dcd9949b2f8
OdedItkinOW <oded.itkin@overwolf.com>
2023-09-21 17:25:41 +0300

    Clarified services

--- website/src/components/home-page/services/services.jsx
+++ website/src/components/home-page/services/services.jsx
@@ -1,89 +1,132 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
+
 import './services.scss';
 
 const Services = (props) => {
- const {} = props;
+  const {} = props;
 
- const [appBtn, seAppBtn] = useState(false);
- const [electronAppBtn, seElectronAppBtn] = useState(false);
+  const [appBtn, seAppBtn] = useState(false);
+  const [electronAppBtn, seElectronAppBtn] = useState(false);
 
-// ---------------------------------------------------------------------------
+  // ---------------------------------------------------------------------------
 
   const handleToggleApp = () => {
-    seAppBtn(current => !current);
-  }
+    seAppBtn((current) => !current);
+  };
 
   const handleToggleElectronApp = () => {
-    seElectronAppBtn(current => !current);
-  }
+    seElectronAppBtn((current) => !current);
+  };
 
   return (
     <section className="services-section">
-      <ul className='services-inner'>
+      <ul className="services-inner">
         <li className={`service-item ${appBtn ? 'btn-hover' : ''}`}>
-          <div className='assets'>
+          <div className="assets">
             <img
-              src='/img/home-2023/platform-app-bg.webp'
-              alt='Overwolf Platform App'
-              className='platform-app-bg bg'
-              width={512} height={280} />
-            <img src='/img/home-2023/platform-app.webp' alt='Overwolf Platform App' className='platform-app symbol' />
+              src="/img/home-2023/platform-app-bg.webp"
+              alt="Overwolf Platform App"
+              className="platform-app-bg bg"
+              width={512}
+              height={280}
+            />
+            <img
+              src="/img/home-2023/platform-app.webp"
+              alt="Overwolf Platform App"
+              className="platform-app symbol"
+            />
           </div>
-          <div className='info'>
+          <div className="info">
             <h2>Overwolf Platform</h2>
-            <p>Create powerful desktop apps and game overlays, built with 100% pure JavaScript.</p>
-            <a href='/start/getting-started'
-              className='btn-primary'
+            <p>
+              Create powerful desktop apps and game overlays, built with 100%
+              pure JavaScript.
+            </p>
+            <a
+              href="/start/getting-started"
+              className="btn-primary"
               onMouseEnter={handleToggleApp}
-              onMouseLeave={handleToggleApp}>
-              Explore</a>
-            <ul className='app-info-list'>
+              onMouseLeave={handleToggleApp}
+            >
+              Explore
+            </a>
+            <ul className="app-info-list">
               <li>
-                <svg><use href="/img/sprite.svg#monetization" /></svg>
-                Monetization
+                <svg>
+                  <use href="/img/sprite.svg#monetization" />
+                </svg>
+                Monetization (Ads, Subscriptions)
               </li>
               <li>
-                <svg><use href="/img/sprite.svg#in-game-overlay" /></svg>
+                <svg>
+                  <use href="/img/sprite.svg#in-game-overlay" />
+                </svg>
                 In-game overlay
               </li>
               <li>
-                <svg><use href="/img/sprite.svg#real-time-game-events" /></svg>
-                Real time game data
+                <svg>
+                  <use href="/img/sprite.svg#real-time-game-events" />
+                </svg>
+                Real time Game Data for &gt; 40 Games
               </li>
             </ul>
           </div>
         </li>
 
         <li className={`service-item ${electronAppBtn ? 'btn-hover' : ''}`}>
-          <div className='assets'>
+          <div className="assets">
             <img
-              src='/img/home-2023/electron-app-bg.webp'
-              alt='Overwolf Electron App'
-              className='electron-app-bg bg'
-              width={512} height={280} />
-            <img src='/img/home-2023/electron-app.webp' alt='Overwolf Electron App' className='electron-app symbol' />
+              src="/img/home-2023/electron-app-bg.webp"
+              alt="Overwolf Electron App"
+              className="electron-app-bg bg"
+              width={512}
+              height={280}
+            />
+            <img
+              src="/img/home-2023/electron-app.webp"
+              alt="Overwolf Electron App"
+              className="electron-app symbol"
+            />
           </div>
-          <div className='info'>
+          <div className="info">
             <h2>Overwolf Electron</h2>
-            <p>Leverage an empowered <a target="_blank" href='https://www.electronjs.org/'>Electron.js</a> framework, to make your existing apps even better!</p>
-            <a href='/tools/ow-electron'
-              className='btn-primary'
+            <p>
+              Leverage an empowered{' '}
+              <a
+                target="_blank"
+                href="https://www.electronjs.org/"
+                rel="noreferrer"
+              >
+                Electron.js
+              </a>{' '}
+              framework, to make your existing apps even better!
+            </p>
+            <a
+              href="/tools/ow-electron"
+              className="btn-primary"
               onMouseEnter={handleToggleElectronApp}
-              onMouseLeave={handleToggleElectronApp}>
-              Explore</a>
-            <ul className='app-info-list'>
+              onMouseLeave={handleToggleElectronApp}
+            >
+              Explore
+            </a>
+            <ul className="app-info-list">
               <li>
-                <svg><use href="/img/sprite.svg#monetization" /></svg>
-                Monetization
+                <svg>
+                  <use href="/img/sprite.svg#monetization" />
+                </svg>
+                Monetization (Ads, Subscriptions)
               </li>
               <li>
-                <svg><use href="/img/sprite.svg#in-game-overlay" /></svg>
+                <svg>
+                  <use href="/img/sprite.svg#in-game-overlay" />
+                </svg>
                 In-game overlay
               </li>
               <li>
-                <svg><use href="/img/sprite.svg#real-time-game-events" /></svg>
-                Real time game data
+                <svg>
+                  <use href="/img/sprite.svg#real-time-game-events" />
+                </svg>
+                Real time Game Data for &gt; 40 Games
               </li>
             </ul>
           </div>
@@ -91,7 +134,6 @@ const Services = (props) => {
       </ul>
     </section>
   );
-
-}
+};
 
 export default Services;
--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -6,10 +6,13 @@ import Documentation from '../components/home-page/documentation/documentation';
 import Community from '../components/home-page/community/community';
 
 const Index = (props) => {
-  const {} = props;
+  // const {} = props;
 
   return (
-    <Layout title="Easily create apps for PC games on the Overwolf framework" description="Easily create apps for PC games on the Overwolf framework">
+    <Layout
+      title="Easily create apps for PC games on the Overwolf framework"
+      description="Easily create apps for PC games on the Overwolf framework"
+    >
       <main id="content" className="main-content">
         <Hero />
         <Services />
@@ -18,7 +21,6 @@ const Index = (props) => {
       </main>
     </Layout>
   );
+};
 
- }
-
- export default Index;
+export default Index;
