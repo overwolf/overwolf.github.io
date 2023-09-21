f7e9f766fb3f186c9cf98fe0a05f532f0f1eadeb
Oded Itkin <OdedItkinOW@overwolf.com>
2023-09-21 07:23:19 +0000

    Deploy website - based on 57495efbfbe87112304c334cd43d5455c8d3ff90

--- js/games_metadata.js
+++ js/games_metadata.js
@@ -3,7 +3,7 @@ const GamesMetaData = {
         "id": 765,
         "path": "/status/world-of-warcraft",
         "name": "World of Warcraft",
-        "docs": "/api/games/events/world-of-warcraft",
+        "docs": "/api/game-data/supported-games/world-of-warcraft",
         "iconUrl": "/img/games-logos/world-of-warcraft.png",
         "iconLargeUrl": "/img/games-logos/large/world-of-warcraft.png"
     },
@@ -13,7 +13,7 @@ const GamesMetaData = {
         "path": "/status/league-of-legends",
         "name": "League of Legends",
         "compliance": "/start/game-compliance/league-of-legends",
-        "docs": "/api/games/events/league-of-legends",
+        "docs": "/api/game-data/supported-games/league-of-legends",
         "iconUrl": "/img/games-logos/league-of-legends.png",
         "iconLargeUrl": "/img/games-logos/large/league-of-legends.png"
     },
@@ -21,7 +21,7 @@ const GamesMetaData = {
         "id": 5855,
         "path": "/status/starcraft-2",
         "name": "StarCraft II",
-        "docs": "/api/games/events/starcraft-2",
+        "docs": "/api/game-data/supported-games/starcraft-2",
         "iconUrl": "/img/games-logos/starcraft-2.png",
         "iconLargeUrl": "/img/games-logos/large/starcraft-2.png"
     },
@@ -29,7 +29,7 @@ const GamesMetaData = {
         "id": 6350,
         "path": "/status/final-fantasy-xiv",
         "name": "Final Fantasy XIV",
-        "docs": "/api/games/events/final-fantasy-xiv",
+        "docs": "/api/game-data/supported-games/final-fantasy-xiv",
         "iconUrl": "/img/games-logos/final-fantasy-xiv.png",
         "iconLargeUrl": "/img/games-logos/large/final-fantasy-xiv.png"
     },
@@ -37,7 +37,7 @@ const GamesMetaData = {
         "id": 6365,
         "path": "/status/world-of-tanks",
         "name": "World of Tanks",
-        "docs": "/api/games/events/world-of-tanks",
+        "docs": "/api/game-data/supported-games/world-of-tanks",
         "iconUrl": "/img/games-logos/world-of-tanks.png",
         "iconLargeUrl": "/img/games-logos/large/world-of-tanks.png"
     },
@@ -45,7 +45,7 @@ const GamesMetaData = {
         "id": 7212,
         "path": "/status/path-of-exile",
         "name": "Path of Exile",
-        "docs": "/api/games/events/path-of-exile",
+        "docs": "/api/game-data/supported-games/path-of-exile",
         "iconUrl": "/img/games-logos/path-of-exile.png",
         "iconLargeUrl": "/img/games-logos/large/path-of-exile.png"
     },
@@ -54,7 +54,7 @@ const GamesMetaData = {
         "path": "/status/dota-2",
         "name": "Dota 2",
         "compliance": "/start/game-compliance/dota-2",
-        "docs": "/api/games/events/dota-2",
+        "docs": "/api/game-data/supported-games/dota-2",
         "iconUrl": "/img/games-logos/dota-2.png",
         "iconLargeUrl": "/img/games-logos/large/dota-2.png"
     },
@@ -62,7 +62,7 @@ const GamesMetaData = {
         "id": 7764,
         "path": "/status/counter-strike-global-offensive",
         "name": "Counter-Strike: Global Offensive",
-        "docs": "/api/games/events/counter-strike-global-offensive",
+        "docs": "/api/game-data/supported-games/counter-strike-global-offensive",
         "iconUrl": "/img/games-logos/counter-strike-global-offensive.png",
         "iconLargeUrl": "/img/games-logos/large/counter-strike-global-offensive.png"
     },
@@ -70,7 +70,7 @@ const GamesMetaData = {
         "id": 8032,
         "path": "/status/minecraft",
         "name": "Minecraft",
-        "docs": "/api/games/events/minecraft",
+        "docs": "/api/game-data/supported-games/minecraft",
         "iconUrl": "/img/games-logos/minecraft.png",
         "iconLargeUrl": "/img/games-logos/large/minecraft.png"
     },
@@ -78,7 +78,7 @@ const GamesMetaData = {
         "id": 8954,
         "path": "/status/warframe",
         "name": "Warframe",
-        "docs": "/api/games/events/warframe",
+        "docs": "/api/game-data/supported-games/warframe",
         "iconUrl": "/img/games-logos/warframe.png",
         "iconLargeUrl": "/img/games-logos/large/warframe.png"
     },
@@ -86,7 +86,7 @@ const GamesMetaData = {
         "id": 9898,
         "path": "/status/hearthstone",
         "name": "Hearthstone",
-        "docs": "/api/games/events/hearthstone",
+        "docs": "/api/game-data/supported-games/hearthstone",
         "iconUrl": "/img/games-logos/hearthstone.png",
         "iconLargeUrl": "/img/games-logos/large/hearthstone.png"
     },
@@ -94,7 +94,7 @@ const GamesMetaData = {
         "id": 10624,
         "path": "/status/heroes-of-the-storm",
         "name": "Heroes of the Storm",
-        "docs": "/api/games/events/heroes-of-the-storm",
+        "docs": "/api/game-data/supported-games/heroes-of-the-storm",
         "iconUrl": "/img/games-logos/heroes-of-the-storm.png",
         "iconLargeUrl": "/img/games-logos/large/heroes-of-the-storm.png"
     },
@@ -102,7 +102,7 @@ const GamesMetaData = {
         "id": 10746,
         "path": "/status/world-of-warships",
         "name": "World of Warships",
-        "docs": "/api/games/events/world-of-warships",
+        "docs": "/api/game-data/supported-games/world-of-warships",
         "iconUrl": "/img/games-logos/world-of-warships.png",
         "iconLargeUrl": "/img/games-logos/large/world-of-warships.png"
     },
@@ -110,7 +110,7 @@ const GamesMetaData = {
         "id": 10798,
         "path": "/status/rocket-league",
         "name": "Rocket League",
-        "docs": "/api/games/events/rocket-league",
+        "docs": "/api/game-data/supported-games/rocket-league",
         "iconUrl": "/img/games-logos/rocket-league.png",
         "iconLargeUrl": "/img/games-logos/large/rocket-league.png"
     },
@@ -119,7 +119,7 @@ const GamesMetaData = {
         "path": "/status/rainbow-six-siege",
         "name": "Rainbow Six Siege",
         "compliance": "/start/game-compliance/rainbow-six-siege",
-        "docs": "/api/games/events/rainbow-six-siege",
+        "docs": "/api/game-data/supported-games/rainbow-six-siege",
         "iconUrl": "/img/games-logos/rainbow-six-siege.png",
         "iconLargeUrl": "/img/games-logos/large/rainbow-six-siege.png"
     },
@@ -127,7 +127,7 @@ const GamesMetaData = {
         "id": 10844,
         "path": "/status/overwatch-2",
         "name": "Overwatch 2",
-        "docs": "/api/games/events/overwatch-2",
+        "docs": "/api/game-data/supported-games/overwatch-2",
         "iconUrl": "/img/games-logos/overwatch-2.png",
         "iconLargeUrl": "/img/games-logos/large/overwatch-2.png"
     },
@@ -142,7 +142,7 @@ const GamesMetaData = {
         ],
         "path": "/status/launchers/league-of-legends",
         "name": "League of Legends Launcher",
-        "docs": "/api/games/launchers/events/league-of-legends",
+        "docs": "/api/game-data/supported-launchers/league-of-legends",
         "iconUrl": "/img/games-logos/launchers/league-of-legends.png",
         "iconLargeUrl": "/img/games-logos/large/launchers/league-of-legends.png"
     },
@@ -150,7 +150,7 @@ const GamesMetaData = {
         "id": 10906,
         "path": "/status/player-unknowns-battlegrounds",
         "name": "PlayerUnknown's Battlegrounds",
-        "docs": "/api/games/events/player-unknowns-battlegrounds",
+        "docs": "/api/game-data/supported-games/player-unknowns-battlegrounds",
         "iconUrl": "/img/games-logos/player-unknowns-battlegrounds.png",
         "iconLargeUrl": "/img/games-logos/large/player-unknowns-battlegrounds.png"
     },
@@ -158,7 +158,7 @@ const GamesMetaData = {
         "id": 21216,
         "path": "/status/fortnite",
         "name": "Fortnite",
-        "docs": "/api/games/events/fortnite",
+        "docs": "/api/game-data/supported-games/fortnite",
         "iconUrl": "/img/games-logos/fortnite.png",
         "iconLargeUrl": "/img/games-logos/large/fortnite.png"
     },
@@ -166,7 +166,7 @@ const GamesMetaData = {
         "id": 21308,
         "path": "/status/magic-the-gathering-arena",
         "name": "Magic: The Gathering Arena",
-        "docs": "/api/games/events/magic-the-gathering-arena",
+        "docs": "/api/game-data/supported-games/magic-the-gathering-arena",
         "iconUrl": "/img/games-logos/magic-the-gathering-arena.png",
         "iconLargeUrl": "/img/games-logos/large/magic-the-gathering-arena.png"
     },
@@ -174,7 +174,7 @@ const GamesMetaData = {
         "id": 21404,
         "path": "/status/splitgate-arena-warfare",
         "name": "Splitgate: Arena Warfare",
-        "docs": "/api/games/events/splitgate-arena-warfare",
+        "docs": "/api/game-data/supported-games/splitgate-arena-warfare",
         "iconUrl": "/img/games-logos/splitgate-arena-warfare.png",
         "iconLargeUrl": "/img/games-logos/large/splitgate-arena-warfare.png"
     },
@@ -187,7 +187,7 @@ const GamesMetaData = {
         ],
         "path": "/status/league-of-legends-arena",
         "name": "League of Legends: Arena",
-        "docs": "/api/games/events/league-of-legends-arena",
+        "docs": "/api/game-data/supported-games/league-of-legends-arena",
         "iconUrl": "/img/games-logos/league-of-legends-arena.png",
         "iconLargeUrl": "/img/games-logos/large/league-of-legends-arena.png"
     },
@@ -195,7 +195,7 @@ const GamesMetaData = {
         "id": 21566,
         "path": "/status/apex-legends",
         "name": "Apex Legends",
-        "docs": "/api/games/events/apex-legends",
+        "docs": "/api/game-data/supported-games/apex-legends",
         "iconUrl": "/img/games-logos/apex-legends.png",
         "iconLargeUrl": "/img/games-logos/large/apex-legends.png"
     },
@@ -208,9 +208,8 @@ const GamesMetaData = {
         ],
         "path": "/status/teamfight-tactics",
         "name": "Teamfight Tactics",
-        "electron": "/api/electron/game-events//api/games/events/teamfight-tactics",
         "compliance": "/start/game-compliance/teamfight-tactics",
-        "docs": "/api/games/events/teamfight-tactics",
+        "docs": "/api/game-data/supported-games/teamfight-tactics",
         "iconUrl": "/img/games-logos/teamfight-tactics.png",
         "iconLargeUrl": "/img/games-logos/large/teamfight-tactics.png"
     },
@@ -218,7 +217,7 @@ const GamesMetaData = {
         "id": 21620,
         "path": "/status/legends-of-runeterra",
         "name": "Legends of Runeterra",
-        "docs": "/api/games/events/legends-of-runeterra",
+        "docs": "/api/game-data/supported-games/legends-of-runeterra",
         "iconUrl": "/img/games-logos/legends-of-runeterra.png",
         "iconLargeUrl": "/img/games-logos/large/legends-of-runeterra.png"
     },
@@ -227,7 +226,7 @@ const GamesMetaData = {
         "path": "/status/call-of-duty-warzone-caldera",
         "name": "Call of Duty: Warzone Caldera",
         "compliance": "/start/game-compliance/call-of-duty-warzone-caldera",
-        "docs": "/api/games/events/call-of-duty-warzone-caldera",
+        "docs": "/api/game-data/supported-games/call-of-duty-warzone-caldera",
         "iconUrl": "/img/games-logos/call-of-duty-warzone-caldera.png",
         "iconLargeUrl": "/img/games-logos/large/call-of-duty-warzone-caldera.png"
     },
@@ -235,7 +234,7 @@ const GamesMetaData = {
         "id": 21634,
         "path": "/status/escape-from-tarkov",
         "name": "Escape From Tarkov",
-        "docs": "/api/games/events/escape-from-tarkov",
+        "docs": "/api/game-data/supported-games/escape-from-tarkov",
         "iconUrl": "/img/games-logos/escape-from-tarkov.png",
         "iconLargeUrl": "/img/games-logos/large/escape-from-tarkov.png"
     },
@@ -243,7 +242,7 @@ const GamesMetaData = {
         "id": 21640,
         "path": "/status/valorant",
         "name": "Valorant",
-        "docs": "/api/games/events/valorant",
+        "docs": "/api/game-data/supported-games/valorant",
         "iconUrl": "/img/games-logos/valorant.png",
         "iconLargeUrl": "/img/games-logos/large/valorant.png"
     },
@@ -251,7 +250,7 @@ const GamesMetaData = {
         "id": 21668,
         "path": "/status/valheim",
         "name": "Valheim",
-        "docs": "/api/games/events/valheim",
+        "docs": "/api/game-data/supported-games/valheim",
         "iconUrl": "/img/games-logos/valheim.png",
         "iconLargeUrl": "/img/games-logos/large/valheim.png"
     },
@@ -259,7 +258,7 @@ const GamesMetaData = {
         "id": 21672,
         "path": "/status/eternal-return",
         "name": "Eternal Return",
-        "docs": "/api/games/events/eternal-return",
+        "docs": "/api/game-data/supported-games/eternal-return",
         "iconUrl": "/img/games-logos/eternal-return.png",
         "iconLargeUrl": "/img/games-logos/large/eternal-return.png"
     },
@@ -268,7 +267,7 @@ const GamesMetaData = {
         "path": "/status/new-world",
         "name": "New World",
         "compliance": "/start/game-compliance/new-world",
-        "docs": "/api/games/events/new-world",
+        "docs": "/api/game-data/supported-games/new-world",
         "iconUrl": "/img/games-logos/new-world.png",
         "iconLargeUrl": "/img/games-logos/large/new-world.png"
     },
@@ -276,7 +275,7 @@ const GamesMetaData = {
         "id": 21848,
         "path": "/status/diablo-2-resurrected",
         "name": "Diablo 2 Resurrected",
-        "docs": "/api/games/events/diablo-2-resurrected",
+        "docs": "/api/game-data/supported-games/diablo-2-resurrected",
         "iconUrl": "/img/games-logos/diablo-2-resurrected.png",
         "iconLargeUrl": "/img/games-logos/large/diablo-2-resurrected.png"
     },
@@ -284,7 +283,7 @@ const GamesMetaData = {
         "id": 21854,
         "path": "/status/halo-infinite",
         "name": "Halo Infinite",
-        "docs": "/api/games/events/halo-infinite",
+        "docs": "/api/game-data/supported-games/halo-infinite",
         "iconUrl": "/img/games-logos/halo-infinite.png",
         "iconLargeUrl": "/img/games-logos/large/halo-infinite.png"
     },
@@ -292,7 +291,7 @@ const GamesMetaData = {
         "id": 21864,
         "path": "/status/lost-ark",
         "name": "Lost Ark",
-        "docs": "/api/games/events/lost-ark",
+        "docs": "/api/game-data/supported-games/lost-ark",
         "iconUrl": "/img/games-logos/lost-ark.png",
         "iconLargeUrl": "/img/games-logos/large/lost-ark.png"
     },
@@ -300,7 +299,7 @@ const GamesMetaData = {
         "id": 21876,
         "path": "/status/call-of-duty-vanguard",
         "name": "Call of Duty: Vanguard",
-        "docs": "/api/games/events/call-of-duty-vanguard",
+        "docs": "/api/game-data/supported-games/call-of-duty-vanguard",
         "iconUrl": "/img/games-logos/call-of-duty-vanguard.png",
         "iconLargeUrl": "/img/games-logos/large/call-of-duty-vanguard.png"
     },
@@ -308,7 +307,7 @@ const GamesMetaData = {
         "id": 21960,
         "path": "/status/axie-infinity-origin",
         "name": "Axie Infinity Origin",
-        "docs": "/api/games/events/axie-infinity-origin",
+        "docs": "/api/game-data/supported-games/axie-infinity-origin",
         "iconUrl": "/img/games-logos/axie-infinity-origin.png",
         "iconLargeUrl": "/img/games-logos/large/axie-infinity-origin.png"
     },
@@ -316,7 +315,7 @@ const GamesMetaData = {
         "id": 22312,
         "path": "/status/leap",
         "name": "LEAP",
-        "docs": "/api/games/events/leap",
+        "docs": "/api/game-data/supported-games/leap",
         "iconUrl": "/img/games-logos/leap.png",
         "iconLargeUrl": "/img/games-logos/large/leap.png"
     },
@@ -324,7 +323,7 @@ const GamesMetaData = {
         "id": 22638,
         "path": "/status/sons-of-the-forest",
         "name": "Sons of the Forest",
-        "docs": "/api/games/events/sons-of-the-forest",
+        "docs": "/api/game-data/supported-games/sons-of-the-forest",
         "iconUrl": "/img/games-logos/sons-of-the-forest.png",
         "iconLargeUrl": "/img/games-logos/large/sons-of-the-forest.png"
     },
@@ -332,7 +331,7 @@ const GamesMetaData = {
         "id": 22700,
         "path": "/status/diablo-4",
         "name": "Diablo 4",
-        "docs": "/api/games/events/diablo-4",
+        "docs": "/api/game-data/supported-games/diablo-4",
         "iconUrl": "/img/games-logos/diablo-4.png",
         "iconLargeUrl": "/img/games-logos/large/diablo-4.png"
     },
@@ -341,7 +340,7 @@ const GamesMetaData = {
         "displayId": 22848,
         "launcher": 10902,
         "mainVariant": 21556,
-        "docs": "/api/games/events/league-of-legends-arena",
+        "docs": "/api/game-data/supported-games/league-of-legends-arena",
         "path": "/status/league-of-legends-public-beta-arena",
         "name": "League of Legends PBE: Arena",
         "variant": "PBE",
@@ -353,10 +352,9 @@ const GamesMetaData = {
         "displayId": 22848,
         "launcher": 10902,
         "mainVariant": 21570,
-        "docs": "/api/games/events/teamfight-tactics",
+        "docs": "/api/game-data/supported-games/teamfight-tactics",
         "path": "/status/teamfight-tactics-public-beta",
         "name": "Teamfight Tactics PBE",
-        "electron": "/api/electron/game-events//api/games/events/teamfight-tactics",
         "variant": "PBE",
         "iconUrl": "/img/games-logos/teamfight-tactics-public-beta.png",
         "iconLargeUrl": "/img/games-logos/large/teamfight-tactics-public-beta.png"
