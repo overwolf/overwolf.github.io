215a16c0cd1b7d93c33e179b6f7c5664bbc90321
benjokoren-ow <ben.koren@overwolf.com>
2023-11-08 14:40:00 +0200

    table header

--- website/src/components/home-page/electron-vs-client-table/electron-vs-client-table.tsx
+++ website/src/components/home-page/electron-vs-client-table/electron-vs-client-table.tsx
@@ -19,7 +19,38 @@ const ElectronVsClientTable: FC<ElectronVsClientTableProps> = props => {
 
   return (
     <section className="electron-vs-client-table-section">
-      <button className="btn-secondary">Open features comparison</button>
+
+      <div className='evc-inner'>
+
+        <div className='evc-table'>
+
+        <div className='table-header'>
+          <h2>Feature Comparison</h2>
+          <h2 className='logo'>
+            <img
+              src="/img/home-2023/native-vs-electron/native-logo-desktop.svg"
+              alt="Overwolf Native"
+              className="native-logo desktop"
+              width={193}
+              height={40}
+            />
+          </h2>
+          <h2 className='logo'>
+            <img
+              src="/img/home-2023/native-vs-electron/electron-logo-desktop.svg"
+              alt="Overwolf Electron"
+              className="electron-logo desktop"
+              width={215}
+              height={40}
+            />
+          </h2>
+        </div>
+
+        </div>
+
+      </div>
+
+      <button className="btn-secondary show-hide-table-btn">Open features comparison</button>
     </section>
   );
 };
