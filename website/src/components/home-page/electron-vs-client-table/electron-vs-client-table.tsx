9344515362e513cb456a7f58956de429ceb04d34
benjokoren-ow <ben.koren@overwolf.com>
2023-11-09 13:44:20 +0200

    light mode and more

--- website/src/components/home-page/electron-vs-client-table/electron-vs-client-table.tsx
+++ website/src/components/home-page/electron-vs-client-table/electron-vs-client-table.tsx
@@ -38,77 +38,89 @@ const ElectronVsClientTable: FC<ElectronVsClientTableProps> = props => {
   // ---------------------------------------------------------------------------
 
   return (
-    <section className={`
-      electron-vs-client-table-section 
-      ${viewType ? 'native-is-shown' : 'electron-is-shown'}
-    `}>
+    <>
+      <div className='table-action-container'>
+      {!showHideTable &&
+          <button
+            className="btn-secondary show-hide-table-btn"
+            onClick={handleToggleTable}
+            >
+              Open features comparison
+          </button>
+        }
+      </div>
 
-      <div className={`evc-inner ${showHideTable ? 'is-open' : ''}`}>
+      <section className={`
+        electron-vs-client-table-section 
+        ${viewType ? 'native-is-shown' : 'electron-is-shown'}
+      `}>
 
-        <div className='evc-table'>
+        <div className={`evc-inner ${showHideTable ? 'is-open' : ''}`}>
 
-          <div className='table-header'>
-            <h2>Feature Comparison</h2>
-            <h2 
-              className='logo native-logo' 
-              onClick={handleToggleViewType}
-              role={elmRole}
-              aria-label='show/hide ow native parameters'
-              >
-              <PlatformLogo native={true} />
-            </h2>
-            <h2 
-              className='logo electron-logo' 
-              onClick={handleToggleViewType}
-              role={elmRole}
-              aria-label='show/hide ow electron parameters'
-              >
-              <PlatformLogo electron={true} />
-            </h2>
+          <div className='evc-table'>
+
+            <div className='table-header'>
+              <h2>Feature Comparison</h2>
+              <h2 
+                className='logo native-logo' 
+                onClick={handleToggleViewType}
+                role={elmRole}
+                aria-label='show/hide ow native parameters'
+                >
+                <PlatformLogo native={true} />
+              </h2>
+              <h2 
+                className='logo electron-logo' 
+                onClick={handleToggleViewType}
+                role={elmRole}
+                aria-label='show/hide ow electron parameters'
+                >
+                <PlatformLogo electron={true} />
+              </h2>
+            </div>
+
+            <h3 className='group-title'>Performance</h3>
+            <EVCrowGroup title='CPU Impact (Idle)' nativeText='0.2%' electronText='0.1%' />
+            <EVCrowGroup title='CPU Impact (Avg Window Interaction)' nativeText='3%' electronText='0.5%' />
+            <EVCrowGroup title='RAM Consumption (Peak)' nativeText='420 MB' electronText='380 MB' />
+            <EVCrowGroup title='Total Running Processes' nativeText='9' electronText='7' />
+
+            <h3 className='group-title'>Growth and Support</h3>
+            <EVCrowGroup title='Analytics' nativeText='Yes' electronText='Yes' green={true} />
+            <EVCrowGroup title='Marketing Support' nativeText='Yes' electronText='Yes' green={true} />
+            <EVCrowGroup title='Funding Support' nativeText='Yes' electronText='Yes' green={true} />
+            <EVCrowGroup title='Product Design and Development' nativeText='Yes' electronText='Yes' green={true} />
+            <EVCrowGroup title='Developer Support' nativeText='Yes' electronText='Yes' green={true} />
+            <EVCrowGroup title='Appstore Presence + Discoverability' nativeText='Yes' electronText='Yes' green={true} />
+
+            <h3 className='group-title'>Tech</h3>
+            <EVCrowGroup title='Overlay SDK (1500+ Games)' nativeText='Yes' electronText='Yes' green={true} />
+            <EVCrowGroup title='Realtime Game Events API' nativeText='Yes' electronText='Yes' green={true} />
+            <EVCrowGroup title='Ad Fraud Protection' nativeText='Yes' electronText='Yes' green={true} />
+            <EVCrowGroup title='Dev Console' nativeText='Yes' electronText='Yes' green={true} />
+            <EVCrowGroup title='Installer' nativeText='Yes' electronText='Optional' nativeGreen={true} />
+            <EVCrowGroup title='Coupled with Overwolf Client' nativeText='Yes' electronText='No' nativeGreen={true} />
+
+            <h3 className='group-title'>Terms and Conditions</h3>
+            <EVCrowGroup title='Ads Revshare' nativeText='70/30' electronText='70/30' />
+            <EVCrowGroup title='Subscriptions Revshare' nativeText='85/15' electronText='85/15' />
+
           </div>
-
-          <h3 className='group-title'>Performance</h3>
-          <EVCrowGroup title='CPU Impact (Idle)' nativeText='0.2%' electronText='0.1%' />
-          <EVCrowGroup title='CPU Impact (Avg Window Interaction)' nativeText='3%' electronText='0.5%' />
-          <EVCrowGroup title='RAM Consumption (Peak)' nativeText='420 MB' electronText='380 MB' />
-          <EVCrowGroup title='Total Running Processes' nativeText='9' electronText='7' />
-
-          <h3 className='group-title'>Growth and Support</h3>
-          <EVCrowGroup title='Analytics' nativeText='Yes' electronText='Yes' green={true} />
-          <EVCrowGroup title='Marketing Support' nativeText='Yes' electronText='Yes' green={true} />
-          <EVCrowGroup title='Funding Support' nativeText='Yes' electronText='Yes' green={true} />
-          <EVCrowGroup title='Product Design and Development' nativeText='Yes' electronText='Yes' green={true} />
-          <EVCrowGroup title='Developer Support' nativeText='Yes' electronText='Yes' green={true} />
-          <EVCrowGroup title='Appstore Presence + Discoverability' nativeText='Yes' electronText='Yes' green={true} />
-
-          <h3 className='group-title'>Tech</h3>
-          <EVCrowGroup title='Overlay SDK (1500+ Games)' nativeText='Yes' electronText='Yes' green={true} />
-          <EVCrowGroup title='Realtime Game Events API' nativeText='Yes' electronText='Yes' green={true} />
-          <EVCrowGroup title='Ad Fraud Protection' nativeText='Yes' electronText='Yes' green={true} />
-          <EVCrowGroup title='Dev Console' nativeText='Yes' electronText='Yes' green={true} />
-          <EVCrowGroup title='Installer' nativeText='Yes' electronText='Optional' nativeGreen={true} />
-          <EVCrowGroup title='Coupled with Overwolf Client' nativeText='Yes' electronText='No' nativeGreen={true} />
-
-          <h3 className='group-title'>Terms and Conditions</h3>
-          <EVCrowGroup title='Ads Revshare' nativeText='70/30' electronText='70/30' />
-          <EVCrowGroup title='Subscriptions Revshare' nativeText='85/15' electronText='85/15' />
 
         </div>
 
-      </div>
+        {showHideTable &&
+          <button
+            className="btn-secondary show-hide-table-btn close-btn"
+            onClick={handleToggleTable}
+            >
+              Close features comparison
+          </button>
+        }
 
-      <button 
-        className="btn-secondary show-hide-table-btn"
-        onClick={handleToggleTable}
-        >
-          {!showHideTable &&
-            <>Open features comparison</>
-          }
-          {showHideTable &&
-            <>Close features comparison</>
-          }
-        </button>
-    </section>
+      </section>
+
+    </>
   );
 };
 
