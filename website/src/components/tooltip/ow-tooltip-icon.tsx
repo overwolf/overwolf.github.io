8173129a54a51e3fc77a769502702d32717cb25a
benjokoren-ow <ben.koren@overwolf.com>
2023-11-09 17:33:58 +0200

    final tooltip logic

--- website/src/components/tooltip/ow-tooltip-icon.tsx
+++ website/src/components/tooltip/ow-tooltip-icon.tsx
@@ -1,6 +1,6 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import './tooltip.scss';
-import { handleMegaToolTipMouseOut, handleMegaToolTipOnOver } from './tooltip-logic';
+import { killAllTooltips, handleMegaToolTip } from './tooltip-logic';
 
 interface OWToolTipIconProps {
   text: string;
@@ -15,20 +15,52 @@ const OWToolTipIcon: FC<OWToolTipIconProps> = props => {
     size = ''
   } = props;
 
+  const className = 'tool-tip-info-icon';
+
+  const onScroll = () => {
+    killAllTooltips()
+  }
+
+  const handleClickOutside = e => {
+    // console.log(e.composedPath())
+    if (e.target.className != className) {
+      killAllTooltips()
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+    document.addEventListener('mousedown', handleClickOutside);
+  }, [])
+  
   // ---------------------------------------------------------------------------
 
   return (
-    <button className='tool-tip-info-icon'
-      data-tooltip={text} 
-      tooltip-position={position} 
-      tooltip-size={size}
-      onMouseOver={handleMegaToolTipOnOver} 
-      onMouseOut={handleMegaToolTipMouseOut}
-      onFocus={handleMegaToolTipOnOver}
-      onBlur={handleMegaToolTipMouseOut}
-      >
-      <svg className='tool-tip-icon'><use href="/img/sprite.svg#info"></use></svg>
-    </button>
+    <>
+      {window.innerWidth < 600 &&
+        <button className='tool-tip-info-icon'
+          data-tooltip={text}
+          tooltip-position={position}
+          tooltip-size={size}
+          onClick={handleMegaToolTip}
+          >
+          <svg className='tool-tip-icon'><use href="/img/sprite.svg#info"></use></svg>
+        </button>
+      }
+      {window.innerWidth > 600 &&
+        <button className={className}
+          data-tooltip={text}
+          tooltip-position={position}
+          tooltip-size={size}
+          onMouseOver={handleMegaToolTip}
+          onMouseOut={killAllTooltips}
+          onFocus={handleMegaToolTip}
+          onBlur={killAllTooltips}
+          >
+          <svg className='tool-tip-icon'><use href="/img/sprite.svg#info"></use></svg>
+        </button>
+      }
+    </>
   );
 
 }
