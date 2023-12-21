905457d0d2e589c29dbc2591adc7255f6e2215ea
Oded Itkin <OdedItkinOW@overwolf.com>
2023-12-21 09:51:37 +0000

    Deploy website - based on 1209e8f4be602f5dd77e78a70f5ea4c3ab8f335d

---
+++ data/supported-games.js
@@ -0,0 +1,404 @@
+const GamesMetaData = {
+    "765": {
+        "id": 765,
+        "path": "/status/world-of-warcraft",
+        "name": "World of Warcraft",
+        "docs": "/api/live-game-data/supported-games/world-of-warcraft",
+        "iconUrl": "/img/games-logos/world-of-warcraft.png",
+        "iconLargeUrl": "/img/games-logos/large/world-of-warcraft.png"
+    },
+    "5426": {
+        "id": 5426,
+        "launcher": 10902,
+        "path": "/status/league-of-legends",
+        "name": "League of Legends",
+        "compliance": "/start/game-compliance/league-of-legends",
+        "docs": "/api/live-game-data/supported-games/league-of-legends",
+        "iconUrl": "/img/games-logos/league-of-legends.png",
+        "iconLargeUrl": "/img/games-logos/large/league-of-legends.png"
+    },
+    "5855": {
+        "id": 5855,
+        "path": "/status/starcraft-2",
+        "name": "StarCraft II",
+        "docs": "/api/live-game-data/supported-games/starcraft-2",
+        "iconUrl": "/img/games-logos/starcraft-2.png",
+        "iconLargeUrl": "/img/games-logos/large/starcraft-2.png"
+    },
+    "6350": {
+        "id": 6350,
+        "path": "/status/final-fantasy-xiv",
+        "name": "Final Fantasy XIV",
+        "docs": "/api/live-game-data/supported-games/final-fantasy-xiv",
+        "iconUrl": "/img/games-logos/final-fantasy-xiv.png",
+        "iconLargeUrl": "/img/games-logos/large/final-fantasy-xiv.png"
+    },
+    "6365": {
+        "id": 6365,
+        "path": "/status/world-of-tanks",
+        "name": "World of Tanks",
+        "docs": "/api/live-game-data/supported-games/world-of-tanks",
+        "iconUrl": "/img/games-logos/world-of-tanks.png",
+        "iconLargeUrl": "/img/games-logos/large/world-of-tanks.png"
+    },
+    "7212": {
+        "id": 7212,
+        "path": "/status/path-of-exile",
+        "name": "Path of Exile",
+        "docs": "/api/live-game-data/supported-games/path-of-exile",
+        "iconUrl": "/img/games-logos/path-of-exile.png",
+        "iconLargeUrl": "/img/games-logos/large/path-of-exile.png"
+    },
+    "7314": {
+        "id": 7314,
+        "path": "/status/dota-2",
+        "name": "Dota 2",
+        "compliance": "/start/game-compliance/dota-2",
+        "docs": "/api/live-game-data/supported-games/dota-2",
+        "iconUrl": "/img/games-logos/dota-2.png",
+        "iconLargeUrl": "/img/games-logos/large/dota-2.png"
+    },
+    "8032": {
+        "id": 8032,
+        "path": "/status/minecraft",
+        "name": "Minecraft",
+        "docs": "/api/live-game-data/supported-games/minecraft",
+        "iconUrl": "/img/games-logos/minecraft.png",
+        "iconLargeUrl": "/img/games-logos/large/minecraft.png"
+    },
+    "8954": {
+        "id": 8954,
+        "path": "/status/warframe",
+        "name": "Warframe",
+        "docs": "/api/live-game-data/supported-games/warframe",
+        "iconUrl": "/img/games-logos/warframe.png",
+        "iconLargeUrl": "/img/games-logos/large/warframe.png"
+    },
+    "9898": {
+        "id": 9898,
+        "path": "/status/hearthstone",
+        "name": "Hearthstone",
+        "docs": "/api/live-game-data/supported-games/hearthstone",
+        "iconUrl": "/img/games-logos/hearthstone.png",
+        "iconLargeUrl": "/img/games-logos/large/hearthstone.png"
+    },
+    "10624": {
+        "id": 10624,
+        "path": "/status/heroes-of-the-storm",
+        "name": "Heroes of the Storm",
+        "docs": "/api/live-game-data/supported-games/heroes-of-the-storm",
+        "iconUrl": "/img/games-logos/heroes-of-the-storm.png",
+        "iconLargeUrl": "/img/games-logos/large/heroes-of-the-storm.png"
+    },
+    "10746": {
+        "id": 10746,
+        "path": "/status/world-of-warships",
+        "name": "World of Warships",
+        "docs": "/api/live-game-data/supported-games/world-of-warships",
+        "iconUrl": "/img/games-logos/world-of-warships.png",
+        "iconLargeUrl": "/img/games-logos/large/world-of-warships.png"
+    },
+    "10798": {
+        "id": 10798,
+        "path": "/status/rocket-league",
+        "name": "Rocket League",
+        "docs": "/api/live-game-data/supported-games/rocket-league",
+        "iconUrl": "/img/games-logos/rocket-league.png",
+        "iconLargeUrl": "/img/games-logos/large/rocket-league.png"
+    },
+    "10826": {
+        "id": 10826,
+        "path": "/status/rainbow-six-siege",
+        "name": "Rainbow Six Siege",
+        "compliance": "/start/game-compliance/rainbow-six-siege",
+        "docs": "/api/live-game-data/supported-games/rainbow-six-siege",
+        "iconUrl": "/img/games-logos/rainbow-six-siege.png",
+        "iconLargeUrl": "/img/games-logos/large/rainbow-six-siege.png"
+    },
+    "10844": {
+        "id": 10844,
+        "path": "/status/overwatch-2",
+        "name": "Overwatch 2",
+        "docs": "/api/live-game-data/supported-games/overwatch-2",
+        "iconUrl": "/img/games-logos/overwatch-2.png",
+        "iconLargeUrl": "/img/games-logos/large/overwatch-2.png"
+    },
+    "10902": {
+        "id": 10902,
+        "games": [
+            5426,
+            21570,
+            215701,
+            21556,
+            215561
+        ],
+        "path": "/status/launchers/league-of-legends",
+        "name": "League of Legends Launcher",
+        "docs": "/api/live-game-data/supported-launchers/league-of-legends",
+        "iconUrl": "/img/games-logos/launchers/league-of-legends.png",
+        "iconLargeUrl": "/img/games-logos/large/launchers/league-of-legends.png"
+    },
+    "10906": {
+        "id": 10906,
+        "path": "/status/player-unknowns-battlegrounds",
+        "name": "PlayerUnknown's Battlegrounds",
+        "docs": "/api/live-game-data/supported-games/player-unknowns-battlegrounds",
+        "iconUrl": "/img/games-logos/player-unknowns-battlegrounds.png",
+        "iconLargeUrl": "/img/games-logos/large/player-unknowns-battlegrounds.png"
+    },
+    "21216": {
+        "id": 21216,
+        "path": "/status/fortnite",
+        "name": "Fortnite",
+        "docs": "/api/live-game-data/supported-games/fortnite",
+        "iconUrl": "/img/games-logos/fortnite.png",
+        "iconLargeUrl": "/img/games-logos/large/fortnite.png"
+    },
+    "21308": {
+        "id": 21308,
+        "path": "/status/magic-the-gathering-arena",
+        "name": "Magic: The Gathering Arena",
+        "docs": "/api/live-game-data/supported-games/magic-the-gathering-arena",
+        "iconUrl": "/img/games-logos/magic-the-gathering-arena.png",
+        "iconLargeUrl": "/img/games-logos/large/magic-the-gathering-arena.png"
+    },
+    "21404": {
+        "id": 21404,
+        "path": "/status/splitgate-arena-warfare",
+        "name": "Splitgate: Arena Warfare",
+        "docs": "/api/live-game-data/supported-games/splitgate-arena-warfare",
+        "iconUrl": "/img/games-logos/splitgate-arena-warfare.png",
+        "iconLargeUrl": "/img/games-logos/large/splitgate-arena-warfare.png"
+    },
+    "21556": {
+        "id": 21556,
+        "displayId": 5426,
+        "launcher": 10902,
+        "subVariants": [
+            215561
+        ],
+        "path": "/status/league-of-legends-arena",
+        "name": "League of Legends: Arena",
+        "docs": "/api/live-game-data/supported-games/league-of-legends-arena",
+        "iconUrl": "/img/games-logos/league-of-legends-arena.png",
+        "iconLargeUrl": "/img/games-logos/large/league-of-legends-arena.png"
+    },
+    "21566": {
+        "id": 21566,
+        "path": "/status/apex-legends",
+        "name": "Apex Legends",
+        "docs": "/api/live-game-data/supported-games/apex-legends",
+        "iconUrl": "/img/games-logos/apex-legends.png",
+        "iconLargeUrl": "/img/games-logos/large/apex-legends.png"
+    },
+    "21570": {
+        "id": 21570,
+        "displayId": 5426,
+        "launcher": 10902,
+        "subVariants": [
+            215701
+        ],
+        "path": "/status/teamfight-tactics",
+        "name": "Teamfight Tactics",
+        "compliance": "/start/game-compliance/teamfight-tactics",
+        "docs": "/api/live-game-data/supported-games/teamfight-tactics",
+        "iconUrl": "/img/games-logos/teamfight-tactics.png",
+        "iconLargeUrl": "/img/games-logos/large/teamfight-tactics.png"
+    },
+    "21620": {
+        "id": 21620,
+        "path": "/status/legends-of-runeterra",
+        "name": "Legends of Runeterra",
+        "docs": "/api/live-game-data/supported-games/legends-of-runeterra",
+        "iconUrl": "/img/games-logos/legends-of-runeterra.png",
+        "iconLargeUrl": "/img/games-logos/large/legends-of-runeterra.png"
+    },
+    "21634": {
+        "id": 21634,
+        "path": "/status/escape-from-tarkov",
+        "name": "Escape From Tarkov",
+        "docs": "/api/live-game-data/supported-games/escape-from-tarkov",
+        "iconUrl": "/img/games-logos/escape-from-tarkov.png",
+        "iconLargeUrl": "/img/games-logos/large/escape-from-tarkov.png"
+    },
+    "21640": {
+        "id": 21640,
+        "path": "/status/valorant",
+        "name": "Valorant",
+        "docs": "/api/live-game-data/supported-games/valorant",
+        "iconUrl": "/img/games-logos/valorant.png",
+        "iconLargeUrl": "/img/games-logos/large/valorant.png"
+    },
+    "21656": {
+        "id": 21656,
+        "path": "/status/genshin-impact",
+        "name": "Genshin Impact",
+        "docs": "/api/live-game-data/supported-games/genshin-impact",
+        "iconUrl": "/img/games-logos/genshin-impact.png",
+        "iconLargeUrl": "/img/games-logos/large/genshin-impact.png"
+    },
+    "21668": {
+        "id": 21668,
+        "path": "/status/valheim",
+        "name": "Valheim",
+        "docs": "/api/live-game-data/supported-games/valheim",
+        "iconUrl": "/img/games-logos/valheim.png",
+        "iconLargeUrl": "/img/games-logos/large/valheim.png"
+    },
+    "21672": {
+        "id": 21672,
+        "path": "/status/eternal-return",
+        "name": "Eternal Return",
+        "docs": "/api/live-game-data/supported-games/eternal-return",
+        "iconUrl": "/img/games-logos/eternal-return.png",
+        "iconLargeUrl": "/img/games-logos/large/eternal-return.png"
+    },
+    "21816": {
+        "id": 21816,
+        "path": "/status/new-world",
+        "name": "New World",
+        "compliance": "/start/game-compliance/new-world",
+        "docs": "/api/live-game-data/supported-games/new-world",
+        "iconUrl": "/img/games-logos/new-world.png",
+        "iconLargeUrl": "/img/games-logos/large/new-world.png"
+    },
+    "21848": {
+        "id": 21848,
+        "path": "/status/diablo-2-resurrected",
+        "name": "Diablo 2 Resurrected",
+        "docs": "/api/live-game-data/supported-games/diablo-2-resurrected",
+        "iconUrl": "/img/games-logos/diablo-2-resurrected.png",
+        "iconLargeUrl": "/img/games-logos/large/diablo-2-resurrected.png"
+    },
+    "21854": {
+        "id": 21854,
+        "path": "/status/halo-infinite",
+        "name": "Halo Infinite",
+        "docs": "/api/live-game-data/supported-games/halo-infinite",
+        "iconUrl": "/img/games-logos/halo-infinite.png",
+        "iconLargeUrl": "/img/games-logos/large/halo-infinite.png"
+    },
+    "21864": {
+        "id": 21864,
+        "path": "/status/lost-ark",
+        "name": "Lost Ark",
+        "docs": "/api/live-game-data/supported-games/lost-ark",
+        "iconUrl": "/img/games-logos/lost-ark.png",
+        "iconLargeUrl": "/img/games-logos/large/lost-ark.png"
+    },
+    "21876": {
+        "id": 21876,
+        "path": "/status/call-of-duty-vanguard",
+        "name": "Call of Duty: Vanguard",
+        "docs": "/api/live-game-data/supported-games/call-of-duty-vanguard",
+        "iconUrl": "/img/games-logos/call-of-duty-vanguard.png",
+        "iconLargeUrl": "/img/games-logos/large/call-of-duty-vanguard.png"
+    },
+    "21960": {
+        "id": 21960,
+        "path": "/status/axie-infinity-origin",
+        "name": "Axie Infinity Origin",
+        "docs": "/api/live-game-data/supported-games/axie-infinity-origin",
+        "iconUrl": "/img/games-logos/axie-infinity-origin.png",
+        "iconLargeUrl": "/img/games-logos/large/axie-infinity-origin.png"
+    },
+    "22088": {
+        "id": 22088,
+        "path": "/status/baldurs-gate-3",
+        "name": "Baldur's Gate 3",
+        "docs": "/api/live-game-data/supported-games/baldurs-gate-3",
+        "iconUrl": "/img/games-logos/baldurs-gate-3.png",
+        "iconLargeUrl": "/img/games-logos/large/baldurs-gate-3.png"
+    },
+    "22312": {
+        "id": 22312,
+        "path": "/status/leap",
+        "name": "LEAP",
+        "docs": "/api/live-game-data/supported-games/leap",
+        "iconUrl": "/img/games-logos/leap.png",
+        "iconLargeUrl": "/img/games-logos/large/leap.png"
+    },
+    "22328": {
+        "id": 22328,
+        "path": "/status/call-of-duty-modern-warfare-2",
+        "name": "Call of Duty: Modern Warfare 2",
+        "docs": "/api/live-game-data/supported-games/call-of-duty-modern-warfare-2",
+        "iconUrl": "/img/games-logos/call-of-duty-modern-warfare-2.png",
+        "iconLargeUrl": "/img/games-logos/large/call-of-duty-modern-warfare-2.png"
+    },
+    "22638": {
+        "id": 22638,
+        "path": "/status/sons-of-the-forest",
+        "name": "Sons of the Forest",
+        "docs": "/api/live-game-data/supported-games/sons-of-the-forest",
+        "iconUrl": "/img/games-logos/sons-of-the-forest.png",
+        "iconLargeUrl": "/img/games-logos/large/sons-of-the-forest.png"
+    },
+    "22700": {
+        "id": 22700,
+        "path": "/status/diablo-4",
+        "name": "Diablo 4",
+        "docs": "/api/live-game-data/supported-games/diablo-4",
+        "iconUrl": "/img/games-logos/diablo-4.png",
+        "iconLargeUrl": "/img/games-logos/large/diablo-4.png"
+    },
+    "22730": {
+        "id": 22730,
+        "path": "/status/counter-strike-2",
+        "name": "Counter-Strike 2",
+        "docs": "/api/live-game-data/supported-games/counter-strike-2",
+        "iconUrl": "/img/games-logos/counter-strike-2.png",
+        "iconLargeUrl": "/img/games-logos/large/counter-strike-2.png"
+    },
+    "23222": {
+        "id": 23222,
+        "path": "/status/starfield",
+        "name": "Starfield",
+        "docs": "/api/live-game-data/supported-games/starfield",
+        "iconUrl": "/img/games-logos/starfield.png",
+        "iconLargeUrl": "/img/games-logos/large/starfield.png"
+    },
+    "23424": {
+        "id": 23424,
+        "path": "/status/call-of-duty-modern-warfare-3",
+        "name": "Call of Duty: Modern Warfare 3",
+        "docs": "/api/live-game-data/supported-games/call-of-duty-modern-warfare-3",
+        "iconUrl": "/img/games-logos/call-of-duty-modern-warfare-3.png",
+        "iconLargeUrl": "/img/games-logos/large/call-of-duty-modern-warfare-3.png"
+    },
+    "215561": {
+        "id": 215561,
+        "displayId": 22848,
+        "launcher": 10902,
+        "mainVariant": 21556,
+        "docs": "/api/live-game-data/supported-games/league-of-legends-arena",
+        "path": "/status/league-of-legends-public-beta-arena",
+        "name": "League of Legends PBE: Arena",
+        "variant": "PBE",
+        "iconUrl": "/img/games-logos/league-of-legends-public-beta-arena.png",
+        "iconLargeUrl": "/img/games-logos/large/league-of-legends-public-beta-arena.png"
+    },
+    "215701": {
+        "id": 215701,
+        "displayId": 22848,
+        "launcher": 10902,
+        "mainVariant": 21570,
+        "docs": "/api/live-game-data/supported-games/teamfight-tactics",
+        "path": "/status/teamfight-tactics-public-beta",
+        "name": "Teamfight Tactics PBE",
+        "variant": "PBE",
+        "iconUrl": "/img/games-logos/teamfight-tactics-public-beta.png",
+        "iconLargeUrl": "/img/games-logos/large/teamfight-tactics-public-beta.png"
+    },
+    "223281": {
+        "id": 223281,
+        "displayId": 22328,
+        "path": "/status/call-of-duty-warzone-2-0",
+        "name": "Call of Duty: Warzone 2.0",
+        "docs": "/api/live-game-data/supported-games/call-of-duty-warzone-2-0",
+        "iconUrl": "/img/games-logos/call-of-duty-warzone-2-0.png",
+        "iconLargeUrl": "/img/games-logos/large/call-of-duty-warzone-2-0.png"
+    }
+}
+
+console.log('GamesMetaData is loaded locally');
